697d7b9fc879bd4c4872d14fe6fbf68166b330b0
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-01-31 15:04:19 +1100

    chore: Document steps of single-hop payment test

--- crates/ln-dlc-node/src/tests/single_hop_payment.rs
+++ crates/ln-dlc-node/src/tests/single_hop_payment.rs
@@ -1,4 +1,14 @@
+//! Do this one first.
+
 #[test]
 fn given_sibling_channel_when_payment_then_can_be_claimed() {
+    // 1. Set up two LN-DLC nodes.
+    // 2. Connect the two nodes.
+    // 3. Fund the Bitcoin wallet of one of the nodes (the payer).
+    // 4. Create channel between them.
+    // 5. Generate an invoice from the payer to the payee.
+    // 6. Pay the invoice.
+    // 7. Claim the payment.
+
     todo!()
 }
