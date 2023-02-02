673d2e3e40d992b21d2694ffb3510f125b209bff
Daniel Karzel <daniel@comit.network>
2023-02-02 11:09:32 +1100

    chore: Initial cleanup
    Remove the initial placeholder example code.

--- mobile/ios/Runner/bridge_generated.h
+++ mobile/ios/Runner/bridge_generated.h
@@ -21,16 +21,10 @@ uintptr_t new_dart_opaque(Dart_Handle handle);
 
 intptr_t init_frb_dart_api_dl(void *obj);
 
-void wire_platform(int64_t port_);
-
-void wire_rust_release_mode(int64_t port_);
-
 void free_WireSyncReturn(WireSyncReturn ptr);
 
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
-    dummy_var ^= ((int64_t) (void*) wire_platform);
-    dummy_var ^= ((int64_t) (void*) wire_rust_release_mode);
     dummy_var ^= ((int64_t) (void*) free_WireSyncReturn);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
     dummy_var ^= ((int64_t) (void*) get_dart_object);
