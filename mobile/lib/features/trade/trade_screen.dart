0d706d051d540524bb5deae6934ee0b55de384f9
Daniel Karzel <daniel@comit.network>
2023-02-07 16:51:36 +1100

    chore: Trade screen basic layout
    Introduces `TradeTabs` that can be re-used throughout the trading screens.

--- mobile/lib/features/trade/trade_screen.dart
+++ mobile/lib/features/trade/trade_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_10101/features/trade/btc_usd_trading_pair_image.dart';
+import 'package:get_10101/features/trade/buy_sell_bottom_sheet.dart';
+import 'package:get_10101/features/trade/candlestick_chart.dart';
+import 'package:get_10101/features/trade/trade_tabs.dart';
 import 'package:get_10101/features/trade/trade_theme.dart';
 
 class TradeScreen extends StatefulWidget {
@@ -16,6 +20,9 @@ class _TradeScreenState extends State<TradeScreen> {
   Widget build(BuildContext context) {
     TradeTheme tradeTheme = Theme.of(context).extension<TradeTheme>()!;
 
+    List<String> orders = List<String>.generate(100, (i) => 'Order $i');
+    List<String> positions = List<String>.generate(100, (i) => 'Position $i');
+
     const RoundedRectangleBorder tradeButtonShape = RoundedRectangleBorder(
       borderRadius: BorderRadius.all(
         Radius.circular(8),
@@ -25,9 +32,49 @@ class _TradeScreenState extends State<TradeScreen> {
     const double tradeButtonWidth = 100.0;
 
     return Scaffold(
-        body: ListView(
-          padding: const EdgeInsets.only(left: 25, right: 25),
-          children: const [Center(child: Text("Trade Screen"))],
+        body: Container(
+          padding: const EdgeInsets.only(left: 15, right: 15),
+          child: Column(
+            children: [
+              Row(
+                children: const [BtcUsdTradingPairImage(), Text("BTC/USD")],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: const [CandlestickChart()],
+              ),
+              Expanded(
+                child: TradeTabs(
+                  tabs: const [
+                    "Positions",
+                    "Orders",
+                  ],
+                  tabBarViewChildren: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: positions.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(positions[index]),
+                        );
+                      },
+                    ),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: orders.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(orders[index]),
+                        );
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         floatingActionButton: Padding(
@@ -40,25 +87,7 @@ class _TradeScreenState extends State<TradeScreen> {
                   child: FloatingActionButton.extended(
                     heroTag: "btnBuy",
                     onPressed: () {
-                      showModalBottomSheet<void>(
-                        useRootNavigator: true,
-                        backgroundColor: Colors.green.shade50,
-                        context: context,
-                        builder: (BuildContext context) {
-                          return SizedBox(
-                            height: 200,
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                children: const <Widget>[
-                                  Text('Buy Sheet'),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      );
+                      modelSheet(context: context, direction: Direction.buy);
                     },
                     label: const Text("Buy"),
                     shape: tradeButtonShape,
@@ -98,4 +127,20 @@ class _TradeScreenState extends State<TradeScreen> {
           ),
         ));
   }
+}
+
+modelSheet({required BuildContext context, required Direction direction}) {
+  showModalBottomSheet<void>(
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(
+        top: Radius.circular(20),
+      ),
+    ),
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    useRootNavigator: true,
+    context: context,
+    builder: (BuildContext context) {
+      return BuySellBottomSheet(direction: direction);
+    },
+  );
 }
