bf77918314b6f72b8651900918d1f1b7fcb47402
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-02-07 11:40:19 +1100

    chore: Remove superfluous comment

--- crates/ln-dlc-node/src/node.rs
+++ crates/ln-dlc-node/src/node.rs
@@ -86,7 +86,7 @@ impl Node {
         network: bitcoin::Network,
         data_dir: String,
         address: SocketAddr,
-        electrs_origin: String, // "tcp://localhost:50000".to_string()
+        electrs_origin: String,
         seed: [u8; 32],
         ephemeral_randomness: [u8; 32],
     ) -> Self {
