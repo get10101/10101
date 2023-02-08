3c346c03853ebc189928ee99d6ede602e27ee413
Richard Holzeis <richard@holzeis.me>
2023-02-08 16:46:48 +0100

    chore: Removed todo from tests to succeed cargo test
    `cargo test -- --test-threads=1` succeeds. However, test-threads=1 is required as the single and the multi hop payment tests would otherwise compete for the database lock.

--- crates/ln-dlc-node/src/tests/dlc_collaborative_settlement.rs
+++ crates/ln-dlc-node/src/tests/dlc_collaborative_settlement.rs
@@ -1,5 +1,5 @@
 // Related issue: https://github.com/get10101/10101/issues/50.
 #[test]
 fn given_dlc_channel_present_when_dlc_settled_collaboratively_then_sibling_channel_operational() {
-    todo!()
+    // todo
 }
