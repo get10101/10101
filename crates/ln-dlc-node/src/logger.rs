f3b36b763e01b42b1fd49f41416c10c2fc337b31
Restioson <restiosondev@gmail.com>
2023-02-08 11:44:43 +0200

    Log rust-lightning records to tracing
    This is done through the `tracing_log` crate to use its dispatch_record function, as it is easier to convert a `lightning::Record` to a `log::Record` and convert it through that crate than to convert directly to a `tracing::Event`.

--- crates/ln-dlc-node/src/logger.rs
+++ crates/ln-dlc-node/src/logger.rs
@@ -1,30 +1,32 @@
+use lightning::util::logger::Level;
 use lightning::util::logger::Logger;
-use lightning::util::logger::Record;
+use lightning::util::logger::Record as LnRecord;
+use tracing_log::log;
+use tracing_log::log::Metadata;
 
 #[derive(Copy, Clone)]
 pub(crate) struct TracingLogger;
 
 impl Logger for TracingLogger {
-    fn log(&self, record: &Record) {
-        match record.level {
-            lightning::util::logger::Level::Gossip => {
-                println!("GOSSIP: {}", record.args.to_string());
-            }
-            lightning::util::logger::Level::Trace => {
-                println!("TRACE: {}", record.args.to_string());
-            }
-            lightning::util::logger::Level::Debug => {
-                println!("DEBUG: {}", record.args.to_string());
-            }
-            lightning::util::logger::Level::Info => {
-                println!("INFO: {}", record.args.to_string());
-            }
-            lightning::util::logger::Level::Warn => {
-                println!("WARN: {}", record.args.to_string());
-            }
-            lightning::util::logger::Level::Error => {
-                println!("ERROR: {}", record.args.to_string());
-            }
-        }
+    fn log(&self, record: &LnRecord) {
+        let level = match record.level {
+            Level::Gossip | Level::Trace => log::Level::Trace,
+            Level::Debug => log::Level::Debug,
+            Level::Info => log::Level::Info,
+            Level::Warn => log::Level::Warn,
+            Level::Error => log::Level::Error,
+        };
+
+        tracing_log::format_trace(
+            &log::Record::builder()
+                .level(level)
+                .args(record.args)
+                .target(record.module_path)
+                .module_path_static(Some(record.module_path))
+                .file_static(Some(record.file))
+                .line(Some(record.line))
+                .build(),
+        )
+        .expect("to be able to format a log record as a trace")
     }
 }
