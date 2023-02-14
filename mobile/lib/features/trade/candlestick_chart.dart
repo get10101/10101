97e127eef3c0df3880fba058ade930bccc6f943e
Daniel Karzel <daniel@comit.network>
2023-02-14 11:17:15 +1100

    feat: candlestick chart example

--- mobile/lib/features/trade/candlestick_chart.dart
+++ mobile/lib/features/trade/candlestick_chart.dart
@@ -1,14 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class CandlestickChart extends StatelessWidget {
+import 'package:candlesticks/candlesticks.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class CandlestickChart extends StatefulWidget {
   const CandlestickChart({super.key});
 
   @override
+  State<CandlestickChart> createState() => _CandlestickChartState();
+}
+
+class _CandlestickChartState extends State<CandlestickChart> {
+  List<Candle> candles = [];
+  bool themeIsDark = false;
+
+  @override
+  void initState() {
+    fetchCandles().then((value) {
+      setState(() {
+        candles = value;
+      });
+    });
+    super.initState();
+  }
+
+  Future<List<Candle>> fetchCandles() async {
+    final uri = Uri.parse("https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h");
+    final res = await http.get(uri);
+    return (jsonDecode(res.body) as List<dynamic>)
+        .map((e) => Candle.fromJson(e))
+        .toList()
+        .reversed
+        .toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SizedBox(
-        height: 200,
-        child: DecoratedBox(
-            decoration: BoxDecoration(color: Colors.grey),
-            child: Center(child: Text("Candlestick Chart"))));
+    return SizedBox(
+      height: 300,
+      child: Candlesticks(
+        candles: candles,
+      ),
+    );
   }
 }
