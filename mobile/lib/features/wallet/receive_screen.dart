2e9ada51fb9c405da28013eefef96e9f3def2a80
Restioson <restiosondev@gmail.com>
2023-02-27 17:54:11 +0200

    feat: Add share payment screen

--- mobile/lib/features/wallet/receive_screen.dart
+++ mobile/lib/features/wallet/receive_screen.dart
@@ -1,11 +1,15 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_10101/features/wallet/application/wallet_service.dart';
 import 'package:get_10101/features/wallet/domain/wallet_info.dart';
 import 'package:get_10101/features/wallet/wallet_change_notifier.dart';
 import 'package:get_10101/features/wallet/wallet_screen.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ReceiveScreen extends StatefulWidget {
   static const route = "${WalletScreen.route}/$subRouteName";
@@ -27,32 +31,129 @@ class _ReceiveScreenState extends State<ReceiveScreen> {
 
     log("Refresh receive screen: ${info.balances.onChain}");
 
+    const EdgeInsets buttonSpacing = EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0);
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Receive")),
+      appBar: AppBar(title: const Text("Receive funds")),
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const SizedBox(height: 50),
-          SelectableText("Address: ${widget.walletService.getNewAddress()}"),
-          Text("Balance: ${info.balances.lightning.sats} / ${info.balances.onChain.sats}"),
-          ElevatedButton(
-              onPressed: () {
-                setState(() async {
-                  String? invoice = await widget.walletService.createInvoice();
-                  if (invoice != null) {
-                    this.invoice = invoice;
-                  }
-                });
-              },
-              child: const Text("Create Invoice")),
-          SelectableText("Invoice: $invoice"),
-          ElevatedButton(
-              onPressed: () async {
-                await widget.walletService.openChannel();
-              },
-              child: const Text("Open Channel!"))
-        ],
+          child: Container(
+        constraints: const BoxConstraints.expand(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 25.0, bottom: 50.0),
+                  child: Text(
+                    "Share payment request",
+                    style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () async {
+                      await widget.walletService.openChannel();
+                    },
+                    child: const Text("Open Channel!")),
+                ElevatedButton(
+                    onPressed: () async {
+                      String? invoice = await widget.walletService.createInvoice();
+                      if (invoice != null) {
+                        setState(() => this.invoice = invoice);
+                      }
+                    },
+                    child: const Text("Create Invoice")),
+                Expanded(
+                  child: Center(
+                    child: QrImage(
+                      data: invoice,
+                      version: QrVersions.auto,
+                      size: 200.0,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+            Row(children: [
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: buttonSpacing,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Clipboard.setData(ClipboardData(text: invoice)).then((_) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Invoice copied to clipboard')));
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Icon(Icons.copy),
+                        ),
+                        Text("Copy"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Padding(
+                  padding: buttonSpacing,
+                  child: OutlinedButton(
+                    onPressed: () => Share.share(invoice),
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Icon(Icons.send),
+                        ),
+                        Text("Share"),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ]),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  "You will see the incoming payment once you send the funds.",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  // Pop both create invoice screen and share invoice screen
+                  GoRouter.of(context).pop();
+                  GoRouter.of(context).pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                ),
+                child: const Text("Done"),
+              ),
+            ),
+            const SizedBox(height: 30.0),
+          ],
+        ),
       )),
     );
   }
--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_foundation
+import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
