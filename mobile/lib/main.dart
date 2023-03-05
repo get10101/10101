5400745d48454916ee373bd31e827da75dddf64b
Philipp Hoenisch <philipp@hoenisch.at>
2023-03-05 19:46:03 +1100

    fix formatting

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -206,7 +206,6 @@ class _TenTenOneAppState extends State<TenTenOneApp> {
       var lastLogin = await rust.api.updateLastLogin();
       FLog.debug(text: "Last login was at ${lastLogin.date}");
 
-
       await walletChangeNotifier.refreshWalletInfo();
     } on FfiException catch (error) {
       FLog.error(text: "Failed to initialise: Error: ${error.message}", exception: error);
