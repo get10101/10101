06a14862e5471de07b1309f6f488c1cc6a57c79b
1010Tom <1010Tom@mailx.site>
2023-03-13 15:25:48 +1100

    feat: impl queryid for direction
    this is needed so that we can write sql queries with filter by direction

--- coordinator/src/orderbook/db/custom_types.rs
+++ coordinator/src/orderbook/db/custom_types.rs
@@ -3,12 +3,14 @@ use diesel::deserialize;
 use diesel::deserialize::FromSql;
 use diesel::pg::Pg;
 use diesel::pg::PgValue;
+use diesel::query_builder::QueryId;
 use diesel::serialize;
 use diesel::serialize::IsNull;
 use diesel::serialize::Output;
 use diesel::serialize::ToSql;
 use diesel::AsExpression;
 use diesel::FromSqlRow;
+use std::any::TypeId;
 use std::io::Write;
 
 #[derive(Debug, Clone, Copy, PartialEq, FromSqlRow, AsExpression, Eq)]
@@ -16,6 +18,15 @@ use std::io::Write;
 pub enum Direction {
     Long,
     Short,
+}
+
+impl QueryId for DirectionType {
+    type QueryId = DirectionType;
+    const HAS_STATIC_QUERY_ID: bool = false;
+
+    fn query_id() -> Option<TypeId> {
+        None
+    }
 }
 
 impl ToSql<DirectionType, Pg> for Direction {
