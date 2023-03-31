a836d5348510e55da115f63e45a29fe674874bd3
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-03-31 12:32:52 +1100

    chore(ln-dlc-node): Set rounding_mod for test based on n_cets
    We're used to thinking about the discretisation of the payout curve in
    terms of number of CETs. We also know that the more CETs, the more
    time it takes to complete what we used to call "contract setup".
    
    We can therefore use the `n_cets` variable to control how fast these
    tests take to complete the "contract setup" phase.

--- crates/ln-dlc-node/src/tests/mod.rs
+++ crates/ln-dlc-node/src/tests/mod.rs
@@ -301,6 +301,9 @@ fn dummy_contract_input(
 ) -> ContractInput {
     let total_collateral = offer_collateral + accept_collateral;
 
+    let n_cets = 100;
+    let rounding_mod = total_collateral / (n_cets + 1);
+
     ContractInput {
         offer_collateral,
         accept_collateral,
@@ -358,7 +361,7 @@ fn dummy_contract_input(
                 rounding_intervals: RoundingIntervals {
                     intervals: vec![RoundingInterval {
                         begin_interval: 0,
-                        rounding_mod: 1,
+                        rounding_mod,
                     }],
                 },
                 difference_params: None,
