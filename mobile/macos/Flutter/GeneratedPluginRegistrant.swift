dd4b27f3dd9ee2a593ae5c06d5763ee59ed50ebd
Richard Holzeis <richard@holzeis.me>
2023-04-11 11:10:11 -0400

    chore: Add shared preferences dependency
    This will allow us to wrap platform-specific persistent storage for simple data. This is useful for less important data like seed backed up, onboarding screen finished, etc. unrelated to our business domain, but required for rendering.

--- mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
