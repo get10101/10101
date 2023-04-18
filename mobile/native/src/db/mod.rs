60a522a1e0c4bbf801bf09c550602cd2992267c1
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-04-18 23:53:23 +1000

    fix(mobile): Improve SQLite DB configuration
    According to our friends at the Diesel Matrix channel, it is important
    to put the `busy_timeout` configuration at the top[1]. I believe this
    fixes https://github.com/get10101/10101/issues/273.
    
    Additionally, I've also added a couple of recommendations w.r.t. the
    WAL journal_mode.
    
    [1]:
    https://matrix.to/#/!lNGJpfiFVovXFJYmwx:matrix.org/$16666135744587xYUIi:gitter.im?via=mozilla.org&via=gitter.im&via=matrix.org.

--- mobile/native/src/db/mod.rs
+++ mobile/native/src/db/mod.rs
@@ -49,17 +49,17 @@ pub struct ConnectionOptions {
 impl r2d2::CustomizeConnection<SqliteConnection, r2d2::Error> for ConnectionOptions {
     fn on_acquire(&self, conn: &mut SqliteConnection) -> Result<(), r2d2::Error> {
         (|| {
-            if self.enable_wal {
-                conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL;")?;
-            }
-            if self.enable_foreign_keys {
-                conn.batch_execute("PRAGMA foreign_keys = ON;")?;
-            }
             if let Some(d) = self.busy_timeout {
                 conn.batch_execute(&format!(
                     "PRAGMA busy_timeout = {};",
                     d.whole_milliseconds()
                 ))?;
+            }
+            if self.enable_wal {
+                conn.batch_execute("PRAGMA journal_mode = WAL; PRAGMA synchronous = NORMAL; PRAGMA wal_autocheckpoint = 1000; PRAGMA wal_checkpoint(TRUNCATE);")?;
+            }
+            if self.enable_foreign_keys {
+                conn.batch_execute("PRAGMA foreign_keys = ON;")?;
             }
             Ok(())
         })()
