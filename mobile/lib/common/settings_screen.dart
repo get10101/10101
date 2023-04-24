b54e45dc6e011b712721c677af117d69ab9da5be
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-04-24 13:43:25 -0400

    feat(mobile): Only show the close channel buttons on regtest
    They're useful for testing corner cases, whilst our mainnet users are still
    encouraged to reach out to us for support.

--- mobile/lib/common/settings_screen.dart
+++ mobile/lib/common/settings_screen.dart
@@ -64,16 +64,23 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   ? FontWeight.bold
                   : FontWeight.normal),
         ),
-        ElevatedButton(
-            onPressed: () {
-              rust.api.closeChannel();
-            },
-            child: const Text("Close channel")),
-        ElevatedButton(
-            onPressed: () {
-              rust.api.forceCloseChannel();
-            },
-            child: const Text("Force-close channel")),
+        Visibility(
+          visible: config.network == "regtest",
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    rust.api.closeChannel();
+                  },
+                  child: const Text("Close channel")),
+              ElevatedButton(
+                  onPressed: () {
+                    rust.api.forceCloseChannel();
+                  },
+                  child: const Text("Force-close channel")),
+            ],
+          ),
+        ),
         const Divider(),
         Text("Trade Settings",
             style: TextStyle(
