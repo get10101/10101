12e23d7e247c8e615e7faff373e03ffaf208b770
Richard Holzeis <richard@holzeis.me>
2023-05-02 09:58:14 +0200

    chore: Add test path enum for channel close

--- crates/ln-dlc-node/src/tests/just_in_time_channel/mod.rs
+++ crates/ln-dlc-node/src/tests/just_in_time_channel/mod.rs
@@ -1,11 +1,18 @@
+mod channel_close;
 mod create;
 mod multiple_payments;
 mod offline_receiver;
 
 #[derive(PartialEq)]
-pub enum TestPath {
+pub enum TestPathFunding {
     // funding through an always on lightning node
-    OnlineFunding,
+    Online,
     // funding through a mobile lightning node (on the same phone)
-    MobileFunding,
+    Mobile,
+}
+
+#[derive(PartialEq)]
+pub enum TestPathChannelClose {
+    Force,
+    Collaborative,
 }
