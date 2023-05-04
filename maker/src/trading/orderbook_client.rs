45cf47f86bfc0113fec41a50f27375d498b0af06
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-05-04 13:42:38 -0400

    chore(maker): Re-use the same reqwest client
    This is the recommended way of using reqwest according to the docs.

--- maker/src/trading/orderbook_client.rs
+++ maker/src/trading/orderbook_client.rs
@@ -5,31 +5,41 @@ use orderbook_commons::OrderResponse;
 use reqwest::Url;
 use uuid::Uuid;
 
-pub async fn post_new_order(url: Url, order: NewOrder) -> Result<OrderResponse> {
-    let url = url.join("/api/orderbook/orders")?;
-    let client = reqwest::Client::new();
-
-    let response = client.post(url).json(&order).send().await?;
-
-    if response.status().as_u16() == 200 {
-        let response = response.json().await?;
-        Ok(response)
-    } else {
-        tracing::error!("Could not create new order");
-        bail!("Could not create new order ")
-    }
+pub struct OrderbookClient {
+    client: reqwest::Client,
 }
 
-pub async fn delete_order(url: Url, order_id: Uuid) -> Result<()> {
-    let url = url.join(format!("/api/orderbook/orders/{order_id}").as_str())?;
-    let client = reqwest::Client::new();
+impl OrderbookClient {
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
 
-    let response = client.delete(url).send().await?;
+    pub async fn post_new_order(&self, url: Url, order: NewOrder) -> Result<OrderResponse> {
+        let url = url.join("/api/orderbook/orders")?;
 
-    if response.status().as_u16() == 200 {
-        Ok(())
-    } else {
-        tracing::error!("Could not delete new order");
-        bail!("Could not create new order ")
+        let response = self.client.post(url).json(&order).send().await?;
+
+        if response.status().as_u16() == 200 {
+            let response = response.json().await?;
+            Ok(response)
+        } else {
+            tracing::error!("Could not create new order");
+            bail!("Could not create new order ")
+        }
+    }
+
+    pub async fn delete_order(&self, url: Url, order_id: Uuid) -> Result<()> {
+        let url = url.join(format!("/api/orderbook/orders/{order_id}").as_str())?;
+
+        let response = self.client.delete(url).send().await?;
+
+        if response.status().as_u16() == 200 {
+            Ok(())
+        } else {
+            tracing::error!("Could not delete new order");
+            bail!("Could not create new order ")
+        }
     }
 }
