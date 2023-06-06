502ad45c02d21c3b53e8af5435b021cc1d653ea5
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-06-06 19:38:03 +1000

    fix: Revert channel lock patch on rust-dlc again
    This update should only remove the functionality related to
    https://github.com/get10101/rust-dlc/commit/d7c1ec5 and
    https://github.com/get10101/rust-lightning/commit/cb1c096e.
    
    This should ideally be a temporary revert. We at least want to verify
    that this helps with https://github.com/get10101/10101/issues/692.

--- crates/ln-dlc-node/src/node/sub_channel_manager.rs
+++ crates/ln-dlc-node/src/node/sub_channel_manager.rs
@@ -1,4 +1,3 @@
-use crate::dlc_custom_signer::CustomSigner;
 use crate::fee_rate_estimator::FeeRateEstimator;
 use crate::ln_dlc_wallet::LnDlcWallet;
 use crate::node::channel_manager::ChannelManager;
@@ -20,7 +19,6 @@ pub type SubChannelManager = sub_channel_manager::SubChannelManager<
     Arc<SystemTimeProvider>,
     Arc<FeeRateEstimator>,
     Arc<DlcManager>,
-    CustomSigner,
 >;
 
 #[autometrics]
