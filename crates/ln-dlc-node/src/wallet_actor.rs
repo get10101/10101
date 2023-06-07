c68a56ba08072dbfaa47eaf971f4cc34300151a8
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-06-07 15:44:19 +0200

    Add sync method - Problems with async started
    Seems like we can't use the async interfaces :(
    
    I went on a different branch and tried to hand-craft an async version, but it
    didn't get me anywhere.
    
    The next thing is to drop the revert and go with sync bdk until then.
    
    The advantage of cached WalletInfo with accessing should still give us pretty
    good results...

--- crates/ln-dlc-node/src/wallet_actor.rs
+++ crates/ln-dlc-node/src/wallet_actor.rs
@@ -42,9 +42,8 @@ impl WalletActor<EsploraBlockchain, Tree> {
     }
 }
 
-impl<B, DB> WalletActor<B, DB>
+impl<DB> WalletActor<EsploraBlockchain, DB>
 where
-    B: Blockchain,
     DB: BatchDatabase,
 {
     #[tracing::instrument(name = "Sync wallet", skip_all, err)]
@@ -77,5 +76,45 @@ where
         };
         tracing::trace!(target : "wallet", sync_time_sec = %now.elapsed().as_secs(), "Wallet sync done");
         Ok(wallet_info)
+    }
+}
+
+#[async_trait]
+impl<B: 'static, DB: 'static> xtra::Actor for WalletActor<B, DB>
+where
+    B: Blockchain + Send,
+    DB: BatchDatabase + Send,
+{
+    type Stop = ();
+    async fn started(&mut self, ctx: &mut xtra::Context<Self>) {
+        let this = ctx.address().expect("self to be alive");
+
+        tokio_extras::spawn(
+            &this.clone(),
+            this.send_interval(SYNC_INTERVAL, || Sync, xtras::IncludeSpan::Always),
+        );
+    }
+
+    async fn stopped(self) -> Self::Stop {}
+}
+
+/// Message to trigger a sync.
+#[derive(Clone, Copy)]
+pub struct Sync;
+
+#[xtra_productivity]
+impl<DB> WalletActor<EsploraBlockchain, DB>
+where
+    DB: BatchDatabase + Send,
+{
+    pub async fn handle_sync(&mut self, _msg: Sync) {
+        let wallet_info_update = match self.sync_internal().await {
+            Ok(wallet_info) => Some(wallet_info),
+            Err(e) => {
+                tracing::warn!("Syncing failed: {:#}", e);
+                None
+            }
+        };
+        self.cache = wallet_info_update;
     }
 }
