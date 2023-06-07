f73f0366cd68ec36b8afe9835a084d242333a915
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-06-07 15:18:12 +0200

    got it compiling
    Still not as great as I'd like it to, but it's not noo bad.

--- crates/ln-dlc-node/src/wallet_actor.rs
+++ crates/ln-dlc-node/src/wallet_actor.rs
@@ -1,9 +1,17 @@
-use std::time::Duration;
-
-use bdk::{blockchain::EsploraBlockchain, sled::Tree, TransactionDetails, Wallet};
+use anyhow::Context;
+use anyhow::Result;
+use async_trait::async_trait;
+use bdk::{
+    blockchain::{Blockchain, EsploraBlockchain},
+    database::BatchDatabase,
+    sled::Tree,
+    wallet::AddressIndex,
+    SyncOptions, TransactionDetails, Wallet,
+};
 use bitcoin::{Address, Amount, Network};
+use std::time::{Duration, Instant};
 use time::OffsetDateTime;
-use xtra::Actor as _;
+use xtra::prelude::*;
 use xtra_productivity::xtra_productivity;
 use xtras::SendInterval;
 
@@ -31,5 +39,43 @@ impl WalletActor<EsploraBlockchain, Tree> {
             blockchain_client,
             cache: None,
         }
+    }
+}
+
+impl<B, DB> WalletActor<B, DB>
+where
+    B: Blockchain,
+    DB: BatchDatabase,
+{
+    #[tracing::instrument(name = "Sync wallet", skip_all, err)]
+    async fn sync_internal(&mut self) -> Result<WalletInfo> {
+        let now = Instant::now();
+        tracing::trace!(target : "wallet", "Wallet sync started");
+
+        self.wallet
+            .sync(&self.blockchain_client, SyncOptions::default())
+            .await
+            .context("Failed to sync wallet")?;
+
+        let balance =
+            tracing::debug_span!("Get wallet balance").in_scope(|| self.wallet.get_balance())?;
+
+        let balance = match self.wallet.network() {
+            Network::Bitcoin => balance.get_spendable(),
+            _ => balance.get_total(),
+        };
+
+        let address = self.wallet.get_address(AddressIndex::LastUnused)?.address;
+        let transactions = self.wallet.list_transactions(false)?;
+
+        let wallet_info = WalletInfo {
+            network: self.wallet.network(),
+            balance: Amount::from_sat(balance),
+            address,
+            last_updated_at: OffsetDateTime::now_utc(),
+            transactions,
+        };
+        tracing::trace!(target : "wallet", sync_time_sec = %now.elapsed().as_secs(), "Wallet sync done");
+        Ok(wallet_info)
     }
 }
