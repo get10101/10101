c999267a1114a99f46cd83980e3482656aad589a
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-06-09 19:44:00 +1000

    chore: Increase log level of rust-lightning

--- coordinator/src/logger.rs
+++ coordinator/src/logger.rs
@@ -25,8 +25,9 @@ pub fn init_tracing(level: LevelFilter, json_format: bool, tokio_console: bool) 
         .add_directive("rustls=warn".parse()?)
         .add_directive("sled=warn".parse()?)
         .add_directive("bdk=warn".parse()?) // bdk is quite spamy on debug
-        .add_directive("lightning::ln::peer_handler=debug".parse()?)
-        .add_directive("lightning::chain=info".parse()?)
+        .add_directive("lightning=trace".parse()?)
+        .add_directive("lightning::ln::peer_handler=trace".parse()?)
+        .add_directive("lightning::chain=trace".parse()?)
         .add_directive("ureq=info".parse()?);
 
     let mut filter = if tokio_console {
