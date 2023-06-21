edcf24af6c04d659e4397eb71933136034706730
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-06-21 12:00:30 +1000

    chore(ln-dlc-node): Remove superfluous log level config in tests
    The `lightning=trace` configuration already implies
    `lightning::chain::channelmonitor=trace`, so the latter is not needed.

--- crates/ln-dlc-node/src/tests/mod.rs
+++ crates/ln-dlc-node/src/tests/mod.rs
@@ -61,7 +61,7 @@ fn init_tracing() {
     TRACING_TEST_SUBSCRIBER.call_once(|| {
         tracing_subscriber::fmt()
             .with_env_filter(
-                "debug,hyper=warn,reqwest=warn,rustls=warn,bdk=info,lightning=trace,sled=info,lightning::chain::channelmonitor=trace,lightning::ln::peer_handler=debug,ureq=info",
+                "debug,hyper=warn,reqwest=warn,rustls=warn,bdk=info,lightning=trace,sled=info,lightning::ln::peer_handler=debug,ureq=info",
             )
             .with_test_writer()
             .init()
