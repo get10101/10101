d599fe5a72e90cddc7bbd17590f472076c348d6f
Richard Holzeis <richard@holzeis.me>
2023-06-27 11:48:04 +0200

    test: Enhance dlc reconnect test reproduce misuse error during closing a position

--- crates/ln-dlc-node/src/tests/dlc/dlc_setup_with_reconnects.rs
+++ crates/ln-dlc-node/src/tests/dlc/dlc_setup_with_reconnects.rs
@@ -118,7 +118,7 @@ async fn reconnecting_during_dlc_channel_setup() {
     .await
     .unwrap();
 
-    // Wait for the `Accepted` message to
+    // Wait for the `Accepted` message to be processed
     tokio::time::sleep(Duration::from_secs(2)).await;
     // The app processes the repeated accept message from the coordinator and sends another confirm
     // message.
@@ -138,6 +138,93 @@ async fn reconnecting_during_dlc_channel_setup() {
         .list_channels()
         .iter()
         .any(|channel| channel.channel_id == channel_details.channel_id));
+
+    // Process the `Finalize` message from the Coordinator.
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &app,
+        coordinator.info.pubkey,
+        SubChannelStateName::Signed,
+    )
+    .await
+    .unwrap();
+
+    let coordinator_settlement_amount = 12_500;
+    app.propose_dlc_channel_collaborative_settlement(
+        channel_details.channel_id,
+        coordinator_settlement_amount,
+    )
+    .await
+    .unwrap();
+
+    // Process the app's `CloseOffer`
+    let sub_channel = wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &coordinator,
+        app.info.pubkey,
+        SubChannelStateName::CloseOffered,
+    )
+    .await
+    .unwrap();
+
+    coordinator
+        .accept_dlc_channel_collaborative_settlement(&sub_channel.channel_id)
+        .unwrap();
+
+    // Process the coordinator's `CloseAccept` and send `CloseConfirm`
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &app,
+        coordinator.info.pubkey,
+        SubChannelStateName::CloseConfirmed,
+    )
+    .await
+    .unwrap();
+
+    // Wait for the `CloseConfirm` message to be delivered
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    app.reconnect(coordinator_info).await.unwrap();
+
+    // Wait for the peers to reconnect and get the `ChannelReestablish` event. During the reconnect
+    // the coordinator will return from `CloseAccepted` to the `Signed` state.
+    tokio::time::sleep(Duration::from_secs(2)).await;
+
+    // The coordinator handles `ReCloseAccept` action. We need this so that the coordinator advances
+    // its state to `CloseAccepted` again, so that it can process the app's repeated
+    // `CloseConfirm` message
+    sub_channel_manager_periodic_check(
+        coordinator.sub_channel_manager.clone(),
+        &coordinator.dlc_message_handler,
+    )
+    .await
+    .unwrap();
+
+    // Wait for the `CloseAccepted` message to be processed
+    tokio::time::sleep(Duration::from_secs(2)).await;
+    // The app processes the repeated `CloseAccept` message from the coordinator and sends another
+    // `CloseConfirm` message.
+    app.process_incoming_messages().unwrap();
+
+    // Process the app's `CloseConfirm` and send `CloseFinalize`
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &coordinator,
+        app.info.pubkey,
+        SubChannelStateName::OffChainClosed,
+    )
+    .await
+    .unwrap();
+
+    // Process the coordinator's `CloseFinalize`
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &app,
+        coordinator.info.pubkey,
+        SubChannelStateName::OffChainClosed,
+    )
+    .await
+    .unwrap();
 }
 
 #[tokio::test(flavor = "multi_thread")]
