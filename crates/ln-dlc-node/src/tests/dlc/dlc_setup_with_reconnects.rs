f31b98fc90168532971ca49f95c975b763b0579e
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-06-28 20:51:50 +1000

    chore(ln-dlc-node): Reproduce SubChannelCloseFinalize reconnect bug
    We temporarily return to an older version of `rust-dlc` to demonstrate
    that another elusive bug was actually happening before upgrading.

--- crates/ln-dlc-node/src/tests/dlc/dlc_setup_with_reconnects.rs
+++ crates/ln-dlc-node/src/tests/dlc/dlc_setup_with_reconnects.rs
@@ -1,11 +1,14 @@
 use crate::node::dlc_channel::sub_channel_manager_periodic_check;
 use crate::node::Node;
+use crate::tests::dlc::create::create_dlc_channel;
 use crate::tests::dummy_contract_input;
 use crate::tests::init_tracing;
 use crate::tests::wait_until_dlc_channel_state;
 use crate::tests::SubChannelStateName;
 use anyhow::Context;
 use bitcoin::Amount;
+use dlc_manager::subchannel::SubChannelState;
+use dlc_manager::Storage;
 use std::time::Duration;
 
 #[tokio::test(flavor = "multi_thread")]
@@ -120,4 +123,119 @@ async fn reconnecting_during_dlc_channel_setup() {
         .list_channels()
         .iter()
         .any(|channel| channel.channel_id == channel_details.channel_id));
+}
+
+#[tokio::test(flavor = "multi_thread")]
+#[ignore]
+async fn repro_792() {
+    init_tracing();
+
+    // Arrange
+
+    let app_dlc_collateral = 25_000;
+    let coordinator_dlc_collateral = 50_000;
+
+    let app_ln_balance = app_dlc_collateral * 2;
+    let coordinator_ln_balance = coordinator_dlc_collateral * 2;
+
+    let fund_amount = (app_ln_balance + coordinator_ln_balance) * 2;
+
+    let app = Node::start_test_app("app").unwrap();
+    let coordinator = Node::start_test_coordinator("coordinator").unwrap();
+
+    app.connect(coordinator.info).await.unwrap();
+
+    coordinator
+        .fund(Amount::from_sat(fund_amount))
+        .await
+        .unwrap();
+
+    coordinator
+        .open_channel(&app, coordinator_ln_balance, app_ln_balance)
+        .await
+        .unwrap();
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    create_dlc_channel(
+        &coordinator,
+        &app,
+        coordinator_dlc_collateral,
+        app_dlc_collateral,
+    )
+    .await
+    .unwrap();
+
+    let channel_details = coordinator
+        .channel_manager
+        .list_usable_channels()
+        .iter()
+        .find(|c| c.counterparty.node_id == app.info.pubkey)
+        .unwrap()
+        .clone();
+
+    coordinator
+        .propose_dlc_channel_collaborative_settlement(
+            channel_details.channel_id,
+            app_dlc_collateral / 2,
+        )
+        .await
+        .unwrap();
+
+    // Process `CloseOffer`
+    let sub_channel = wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &app,
+        coordinator.info.pubkey,
+        SubChannelStateName::CloseOffered,
+    )
+    .await
+    .unwrap();
+
+    app.accept_dlc_channel_collaborative_settlement(&sub_channel.channel_id)
+        .unwrap();
+
+    // Process `CloseAccept` and send `CloseConfirm`
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &coordinator,
+        app.info.pubkey,
+        SubChannelStateName::CloseConfirmed,
+    )
+    .await
+    .unwrap();
+
+    // Act
+
+    // Process `CloseConfirm` and send `CloseFinalize`
+    wait_until_dlc_channel_state(
+        Duration::from_secs(30),
+        &app,
+        coordinator.info.pubkey,
+        SubChannelStateName::OffChainClosed,
+    )
+    .await
+    .unwrap();
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    app.reconnect(coordinator.info).await.unwrap();
+
+    // The test panics here because of a `Got a revoke commitment secret which didn't correspond to
+    // their current pubkey` error
+    coordinator.process_incoming_messages().unwrap();
+
+    // Assert
+
+    let state = coordinator
+        .dlc_manager
+        .get_store()
+        .get_sub_channels()
+        .unwrap()
+        .first()
+        .unwrap()
+        .state
+        .clone();
+
+    assert!(matches!(state, SubChannelState::OffChainClosed));
 }
