944a8189dd1fad1c36b12ccb4fcf9bf7a0d5ae3b
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-07-04 11:24:29 +0930

    chore(tests-e2e): Move mining and syncing workaround inside funding

--- crates/tests-e2e/src/fund.rs
+++ crates/tests-e2e/src/fund.rs
@@ -4,8 +4,28 @@ use reqwest::Client;
 use reqwest::Response;
 use tokio::task::spawn_blocking;
 
+use crate::bitcoind::Bitcoind;
+use crate::coordinator::Coordinator;
+
+// TODO: Fetch these from the app
+const FUNDING_TRANSACTION_FEES: u64 = 153;
+
 /// Pay a lightning invoice using an LND faucet
-pub async fn fund_app_with_faucet(client: &Client, funding_amount: u64) -> Result<()> {
+///
+/// Returns the funded amount (in satoshis)
+pub async fn fund_app_with_faucet(
+    coordinator: &Coordinator,
+    client: &Client,
+    funding_amount: u64,
+) -> Result<u64> {
+    let bitcoind = Bitcoind::new(client.clone());
+
+    // FIXME: We mine a block before funding the app to ensure that all
+    // outputs are spendable. This is necessary as the test might otherwise fail due to missing
+    // or unspendable output when broadcasting the funding transaction.
+    bitcoind.mine(1).await?;
+    coordinator.sync_wallet().await?;
+
     let invoice = spawn_blocking(move || {
         api::create_invoice_with_amount(funding_amount).expect("to succeed")
     })
@@ -16,7 +36,14 @@ pub async fn fund_app_with_faucet(client: &Client, funding_amount: u64) -> Resul
 
     // Ensure we sync the wallet info after funding
     spawn_blocking(move || api::refresh_wallet_info().expect("to succeed")).await?;
-    Ok(())
+
+    // FIXME: We mine a block before funding the app to ensure that all
+    // outputs are spendable. This is necessary as the test might otherwise fail due to missing
+    // or unspendable output when broadcasting the funding transaction.
+    bitcoind.mine(1).await?;
+    coordinator.sync_wallet().await?;
+
+    Ok(funding_amount - FUNDING_TRANSACTION_FEES)
 }
 
 async fn pay_with_faucet(client: &Client, invoice: String) -> Result<Response> {
--- crates/tests-e2e/tests/basic.rs
+++ crates/tests-e2e/tests/basic.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use native::api;
 use tests_e2e::app::run_app;
-use tests_e2e::bitcoind::Bitcoind;
 use tests_e2e::coordinator::Coordinator;
 use tests_e2e::fund::fund_app_with_faucet;
 use tests_e2e::http::init_reqwest;
@@ -16,8 +15,6 @@ async fn app_can_be_funded_with_lnd_faucet() -> Result<()> {
     let coordinator = Coordinator::new_local(client.clone());
     assert!(coordinator.is_running().await);
 
-    let bitcoind = Bitcoind::new(client.clone());
-
     let app = run_app().await;
 
     // this is just to showcase we can retrieve value from a SyncReturn
@@ -28,29 +25,14 @@ async fn app_can_be_funded_with_lnd_faucet() -> Result<()> {
     assert_eq!(app.rx.wallet_info().unwrap().balances.on_chain, 0);
     assert_eq!(app.rx.wallet_info().unwrap().balances.lightning, 0);
 
-    // TODO: Remove this when fixed. We mine a block before funding the app to ensure that all
-    // outputs are spendable. This is necessary as the test might otherwise fail due to missing
-    // or unspendable output when broadcasting the funding transaction.
-    bitcoind.mine(1).await?;
-    coordinator.sync_wallet().await?;
-
-    let funding_amount = 50_000;
-    let funding_transaction_fees = 153;
-    fund_app_with_faucet(&client, funding_amount).await?;
-
-    // TODO: Remove this when fixed. We mine a block before funding the app to ensure that all
-    // outputs are spendable. This is necessary as the test might otherwise fail due to missing
-    // or unspendable output when broadcasting the funding transaction.
-    bitcoind.mine(1).await?;
-    coordinator.sync_wallet().await?;
+    let funded_amount = fund_app_with_faucet(&coordinator, &client, 50_000).await?;
 
     assert_eq!(app.rx.wallet_info().unwrap().balances.on_chain, 0);
 
-    // TODO: Asserting here on >= as this test run on the CI can't find a route when trying to pay
+    // FIXME: Asserting here on >= as this test run on the CI can't find a route when trying to pay
     // immediately after claiming a received payment.
-    assert!(
-        app.rx.wallet_info().unwrap().balances.lightning
-            >= funding_amount - funding_transaction_fees
-    );
+    let ln_balance = app.rx.wallet_info().unwrap().balances.lightning;
+    tracing::info!(%funded_amount, %ln_balance, "Successfully funded app with faucet");
+    assert!(ln_balance >= funded_amount);
     Ok(())
 }
