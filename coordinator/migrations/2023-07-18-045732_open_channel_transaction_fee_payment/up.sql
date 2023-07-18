eb292adf1bae6e89a3a38ec802e58731f8a24bfb
Daniel Karzel <daniel@comit.network>
2023-07-18 16:08:45 +1000

    Add index to `channels.funding_txid` database column

--- coordinator/migrations/2023-07-18-045732_open_channel_transaction_fee_payment/up.sql
+++ coordinator/migrations/2023-07-18-045732_open_channel_transaction_fee_payment/up.sql
@@ -3,3 +3,4 @@ ALTER TABLE
     channels
 ADD
     COLUMN open_channel_fee_payment_hash TEXT REFERENCES payments(payment_hash);
+CREATE INDEX IF NOT EXISTS channels_funding_txid ON channels(funding_txid);
