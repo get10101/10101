2e050bdbce21485788163962e3e64a2c30037744
Richard Holzeis <richard@holzeis.me>
2023-07-24 14:32:32 +0200

    chore: Adapted sizes and simplified padding

--- mobile/lib/common/app_bar_wrapper.dart
+++ mobile/lib/common/app_bar_wrapper.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get_10101/common/color.dart';
 import 'package:get_10101/features/trade/settings_screen.dart';
-import 'package:get_10101/features/trade/trade_screen.dart';
-import 'package:get_10101/features/wallet/scanner_screen.dart';
-import 'package:get_10101/features/wallet/settings_screen.dart';
-import 'package:get_10101/features/wallet/wallet_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class AppBarWrapper extends StatelessWidget {
@@ -14,55 +10,32 @@ class AppBarWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentRoute = GoRouterState.of(context).location;
-    const appBarHeight = 32.0;
+    const appBarHeight = 35.0;
 
     var actionButtons = <Widget>[];
-    Widget? leadingButton;
-
-    if (currentRoute == WalletScreen.route) {
-      actionButtons = [
-        IconButton(
-          icon: const Icon(Icons.qr_code_scanner),
-          tooltip: 'Scanner',
-          onPressed: () {
-            context.go(ScannerScreen.route);
-          },
-        )
-      ];
-
-      leadingButton = IconButton(
-        icon: const Icon(Icons.settings),
-        tooltip: 'Settings',
-        onPressed: () {
-          context.go(WalletSettingsScreen.route);
-        },
-      );
-    }
-
-    if (currentRoute == TradeScreen.route) {
-      leadingButton = IconButton(
-        icon: const Icon(Icons.settings),
-        tooltip: 'Settings',
-        onPressed: () {
-          context.go(TradeSettingsScreen.route);
-        },
-      );
-    }
-
-    return AppBar(
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      iconTheme: const IconThemeData(
-          color: tenTenOnePurple,
-          // Without adjustment, the icon appears off-center from the title (logo)
-          size: appBarHeight - 4.0),
-      leading: leadingButton,
-      title: SizedBox(
-        height: appBarHeight,
-        child: Image.asset('assets/10101_logo_icon.png'),
-      ),
-      actions: actionButtons,
+    Widget leadingButton = IconButton(
+      icon: const Icon(Icons.settings),
+      tooltip: 'Settings',
+      onPressed: () {
+        context.go(TradeSettingsScreen.route);
+      },
     );
+
+    return Container(
+        margin: const EdgeInsets.only(left: 2.0),
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          iconTheme: const IconThemeData(
+              color: tenTenOnePurple,
+              // Without adjustment, the icon appears off-center from the title (logo)
+              size: appBarHeight - 8.0),
+          leading: leadingButton,
+          title: SizedBox(
+            height: appBarHeight - 10.0,
+            child: Image.asset('assets/10101_logo_icon.png'),
+          ),
+          actions: actionButtons,
+        ));
   }
 }
