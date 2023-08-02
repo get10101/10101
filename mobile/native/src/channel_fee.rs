f189ba6f7c799c3ea760cf2f9574559bf9c83e51
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-08-02 16:46:59 +1000

    fix(app): Wait longer for capacity to pay for channel-opening fee
    Waiting for 5 seconds proved insufficient in some cases. In theory we
    could wait for as long as we want to as this work will happen in the
    background.
    
    It might still help to redesign this flow so that we just keep
    retrying if we owe the coordinator for opening the JIT channel, rather
    than only trying once after claiming the interceptable payment.

--- mobile/native/src/channel_fee.rs
+++ mobile/native/src/channel_fee.rs
@@ -19,6 +19,8 @@ use std::sync::Mutex;
 use std::time::Duration;
 use tokio::runtime::Handle;
 
+const WAIT_FOR_OUTBOUND_CAPACITY_TIMEOUT: Duration = Duration::from_secs(60);
+
 #[derive(Clone)]
 pub struct ChannelFeePaymentSubscriber {
     open_channel_info: Arc<Mutex<Option<(ChannelId, EsploraTransaction)>>>,
@@ -79,7 +81,8 @@ impl ChannelFeePaymentSubscriber {
             );
             Handle::current()
                 .block_on(self.wait_for_outbound_capacity(channel_id, funding_tx_fees_msats))
-        })?;
+        })
+        .context("Failed during wait for outbound capacity")?;
 
         tracing::debug!(
             "Trying to pay channel opening fees of {} sats",
@@ -153,7 +156,7 @@ impl ChannelFeePaymentSubscriber {
         channel_id: ChannelId,
         funding_tx_fees_msats: u64,
     ) -> Result<()> {
-        tokio::time::timeout(Duration::from_secs(5), async {
+        tokio::time::timeout(WAIT_FOR_OUTBOUND_CAPACITY_TIMEOUT, async {
             loop {
                 let channel_details = match self
                     .channel_manager
