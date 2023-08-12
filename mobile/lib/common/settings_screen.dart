13d1a48b1c03d9c699464526088e8b2f692b84d1
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-08-11 18:36:40 -0700

    feat: Add feedback button

--- mobile/lib/common/settings_screen.dart
+++ mobile/lib/common/settings_screen.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:f_logs/f_logs.dart';
+import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
+import 'package:get_10101/common/feedback.dart';
 import 'package:get_10101/common/scrollable_safe_area.dart';
 import 'package:get_10101/features/trade/settings_screen.dart';
 import 'package:get_10101/features/wallet/settings_screen.dart';
@@ -188,6 +190,18 @@ class _SettingsScreenState extends State<SettingsScreen> {
               Share.shareXFiles([logFile], text: 'Logs from $now');
             },
             child: const Text("Share logs")),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          child: const Text('Provide feedback'),
+          onPressed: () {
+            try {
+              BetterFeedback.of(context).show(submitFeedback);
+            } on Exception catch (e) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Failed to share feedback via email app because: $e')));
+            }
+          },
+        ),
       ])),
     );
   }
