a8e58b418966bbbb6bc9f60c701f62d5f750be71
Richard Holzeis <richard@holzeis.me>
2023-10-05 13:13:35 +0200

    chore: Fix typo in function name

--- coordinator/src/db/positions_helper.rs
+++ coordinator/src/db/positions_helper.rs
@@ -11,7 +11,7 @@ pub fn get_non_expired_positions_joined_with_fcm_token(
 ) -> QueryResult<Vec<(crate::position::models::Position, FcmToken)>> {
     let result = conn.transaction(|conn| {
         let positions = Position::get_all_open_positions(conn)?;
-        joine_with_fcm_token(conn, positions)
+        join_with_fcm_token(conn, positions)
     })?;
     Ok(result)
 }
@@ -23,12 +23,12 @@ pub fn get_positions_joined_with_fcm_token_with_expiry_within(
 ) -> QueryResult<Vec<(crate::position::models::Position, FcmToken)>> {
     let result = conn.transaction(|conn| {
         let positions = Position::get_all_positions_with_expiry_within(conn, start, end)?;
-        joine_with_fcm_token(conn, positions)
+        join_with_fcm_token(conn, positions)
     })?;
     Ok(result)
 }
 
-pub fn joine_with_fcm_token(
+pub fn join_with_fcm_token(
     conn: &mut PgConnection,
     positions: Vec<crate::position::models::Position>,
 ) -> QueryResult<Vec<(crate::position::models::Position, FcmToken)>> {
