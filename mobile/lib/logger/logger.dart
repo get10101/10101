72c6aa4df2d9030590fb34cbd42da21263c524e9
Philipp Hoenisch <philipp@coblox.tech>
2023-10-09 09:12:16 +0200

    fix: log things also in production
    Fixes #1458

--- mobile/lib/logger/logger.dart
+++ mobile/lib/logger/logger.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:get_10101/logger/hybrid_logger.dart';
 import 'package:get_10101/logger/simple_utc_printer.dart';
 import 'package:logger/logger.dart';
@@ -7,6 +8,9 @@ Logger get logger => AppLogger.instance;
 class AppLogger {
   static final Logger _logger = Logger(
       output: HybridOutput(),
+      filter: ProductionFilter(),
+      // in Debug build we want to log everything on trace in production build we log on debug
+      level: kDebugMode ? Level.trace : Level.debug,
       printer: SimpleUTCPrinter(
           // Colorful log messages
           colors: false,
