bc707b34f69566919ff3c6266c68ff958f5d1d2c
Mariusz Klochowicz <mariusz@klochowicz.com>
2023-10-16 13:25:44 -0700

    chore(dart): Move code out from main.dart

---
+++ mobile/lib/common/init_service.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:get_10101/common/amount_denomination_change_notifier.dart';
+import 'package:get_10101/common/service_status_notifier.dart';
+import 'package:get_10101/common/recover_dlc_change_notifier.dart';
+import 'package:get_10101/features/stable/stable_value_change_notifier.dart';
+import 'package:get_10101/features/trade/candlestick_change_notifier.dart';
+import 'package:get_10101/features/trade/order_change_notifier.dart';
+import 'package:get_10101/features/trade/position_change_notifier.dart';
+import 'package:get_10101/features/wallet/application/faucet_service.dart';
+import 'package:get_10101/features/trade/rollover_change_notifier.dart';
+import 'package:get_10101/features/trade/trade_value_change_notifier.dart';
+import 'package:get_10101/features/wallet/application/wallet_service.dart';
+import 'package:get_10101/features/wallet/send/payment_sent_change_notifier.dart';
+import 'package:get_10101/features/wallet/payment_claimed_change_notifier.dart';
+import 'package:get_10101/features/wallet/wallet_change_notifier.dart';
+import 'package:get_10101/features/trade/submit_order_change_notifier.dart';
+import 'package:get_10101/features/trade/application/candlestick_service.dart';
+import 'package:get_10101/features/trade/application/order_service.dart';
+import 'package:get_10101/features/trade/application/position_service.dart';
+import 'package:get_10101/features/trade/application/trade_values_service.dart';
+import 'package:get_10101/features/trade/async_order_change_notifier.dart';
+import 'package:get_10101/common/application/channel_info_service.dart';
+import 'package:get_10101/common/channel_status_notifier.dart';
+import 'package:get_10101/common/domain/background_task.dart';
+import 'package:get_10101/common/domain/service_status.dart';
+import 'package:get_10101/features/trade/domain/order.dart';
+import 'package:get_10101/features/trade/domain/position.dart';
+import 'package:get_10101/features/trade/domain/price.dart';
+import 'package:get_10101/features/wallet/domain/wallet_info.dart';
+import 'package:get_10101/common/application/event_service.dart';
+import 'package:get_10101/logger/logger.dart';
+import 'package:nested/nested.dart';
+import 'package:provider/provider.dart';
+import 'package:get_10101/bridge_generated/bridge_definitions.dart' as bridge;
+
+List<SingleChildWidget> createProviders(bridge.Config config) {
+  const ChannelInfoService channelInfoService = ChannelInfoService();
+  var tradeValuesService = TradeValuesService();
+
+  var providers = [
+    ChangeNotifierProvider(create: (context) {
+      return TradeValuesChangeNotifier(tradeValuesService, channelInfoService);
+    }),
+    ChangeNotifierProvider(create: (context) {
+      return StableValuesChangeNotifier(tradeValuesService, channelInfoService);
+    }),
+    ChangeNotifierProvider(create: (context) => AmountDenominationChangeNotifier()),
+    ChangeNotifierProvider(create: (context) => SubmitOrderChangeNotifier(OrderService())),
+    ChangeNotifierProvider(create: (context) => OrderChangeNotifier(OrderService())),
+    ChangeNotifierProvider(create: (context) => PositionChangeNotifier(PositionService())),
+    ChangeNotifierProvider(create: (context) => WalletChangeNotifier(const WalletService())),
+    ChangeNotifierProvider(
+        create: (context) => CandlestickChangeNotifier(const CandlestickService())),
+    ChangeNotifierProvider(create: (context) => ServiceStatusNotifier()),
+    ChangeNotifierProvider(create: (context) => ChannelStatusNotifier()),
+    ChangeNotifierProvider(create: (context) => AsyncOrderChangeNotifier(OrderService())),
+    ChangeNotifierProvider(create: (context) => RolloverChangeNotifier()),
+    ChangeNotifierProvider(create: (context) => RecoverDlcChangeNotifier()),
+    ChangeNotifierProvider(create: (context) => PaymentClaimedChangeNotifier()),
+    ChangeNotifierProvider(create: (context) => PaymentChangeNotifier()),
+    Provider(create: (context) => config),
+    Provider(create: (context) => channelInfoService)
+  ];
+  if (config.network == "regtest") {
+    providers.add(Provider(create: (context) => FaucetService()));
+  }
+
+  return providers;
+}
+
+/// Forward the events from change notifiers to the Event service
+void subscribeToNotifiers(BuildContext context) {
+  // TODO: Move this code into an "InitService" or similar; we should not have bridge code in the widget
+
+  final EventService eventService = EventService.create();
+
+  final orderChangeNotifier = context.read<OrderChangeNotifier>();
+  final positionChangeNotifier = context.read<PositionChangeNotifier>();
+  final walletChangeNotifier = context.read<WalletChangeNotifier>();
+  final tradeValuesChangeNotifier = context.read<TradeValuesChangeNotifier>();
+  final submitOrderChangeNotifier = context.read<SubmitOrderChangeNotifier>();
+  final serviceStatusNotifier = context.read<ServiceStatusNotifier>();
+  final channelStatusNotifier = context.read<ChannelStatusNotifier>();
+  final stableValuesChangeNotifier = context.read<StableValuesChangeNotifier>();
+  final asyncOrderChangeNotifier = context.read<AsyncOrderChangeNotifier>();
+  final rolloverChangeNotifier = context.read<RolloverChangeNotifier>();
+  final recoverDlcChangeNotifier = context.read<RecoverDlcChangeNotifier>();
+  final paymentClaimedChangeNotifier = context.read<PaymentClaimedChangeNotifier>();
+  final paymentChangeNotifier = context.read<PaymentChangeNotifier>();
+
+  eventService.subscribe(
+      orderChangeNotifier, bridge.Event.orderUpdateNotification(Order.apiDummy()));
+
+  eventService.subscribe(
+      submitOrderChangeNotifier, bridge.Event.orderUpdateNotification(Order.apiDummy()));
+
+  eventService.subscribe(
+      positionChangeNotifier, bridge.Event.positionUpdateNotification(Position.apiDummy()));
+
+  eventService.subscribe(
+      positionChangeNotifier,
+      const bridge.Event.positionClosedNotification(
+          bridge.PositionClosed(contractSymbol: bridge.ContractSymbol.BtcUsd)));
+
+  eventService.subscribe(
+      walletChangeNotifier, bridge.Event.walletInfoUpdateNotification(WalletInfo.apiDummy()));
+
+  eventService.subscribe(
+      tradeValuesChangeNotifier, bridge.Event.priceUpdateNotification(Price.apiDummy()));
+
+  eventService.subscribe(
+      stableValuesChangeNotifier, bridge.Event.priceUpdateNotification(Price.apiDummy()));
+
+  eventService.subscribe(
+      positionChangeNotifier, bridge.Event.priceUpdateNotification(Price.apiDummy()));
+
+  eventService.subscribe(
+      serviceStatusNotifier, bridge.Event.serviceHealthUpdate(serviceUpdateApiDummy()));
+
+  eventService.subscribe(
+      asyncOrderChangeNotifier, bridge.Event.orderUpdateNotification(Order.apiDummy()));
+  eventService.subscribe(
+      asyncOrderChangeNotifier, bridge.Event.backgroundNotification(AsyncTrade.apiDummy()));
+
+  eventService.subscribe(
+      rolloverChangeNotifier, bridge.Event.backgroundNotification(Rollover.apiDummy()));
+
+  eventService.subscribe(
+      recoverDlcChangeNotifier, bridge.Event.backgroundNotification(RecoverDlc.apiDummy()));
+
+  eventService.subscribe(paymentClaimedChangeNotifier, const bridge.Event.paymentClaimed());
+
+  eventService.subscribe(paymentChangeNotifier, const bridge.Event.paymentSent());
+  eventService.subscribe(paymentChangeNotifier, const bridge.Event.paymentFailed());
+
+  channelStatusNotifier.subscribe(eventService);
+
+  eventService.subscribe(
+      AnonSubscriber((event) => logger.i(event.field0)), const bridge.Event.log(""));
+}
---
+++ mobile/lib/common/routes.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:get_10101/common/global_keys.dart';
+import 'package:get_10101/common/loading_screen.dart';
+import 'package:get_10101/common/scaffold_with_nav_bar.dart';
+import 'package:get_10101/features/stable/stable_screen.dart';
+import 'package:get_10101/features/trade/trade_screen.dart';
+import 'package:get_10101/features/wallet/domain/wallet_type.dart';
+import 'package:get_10101/features/wallet/onboarding/onboarding_screen.dart';
+import 'package:get_10101/features/wallet/receive_screen.dart';
+import 'package:get_10101/features/wallet/scanner_screen.dart';
+import 'package:get_10101/features/wallet/seed_screen.dart';
+import 'package:get_10101/features/wallet/send/send_screen.dart';
+import 'package:get_10101/features/wallet/wallet_screen.dart';
+import 'package:get_10101/features/welcome/welcome_screen.dart';
+import 'package:go_router/go_router.dart';
+
+GoRouter createRouter() {
+  return GoRouter(
+      navigatorKey: rootNavigatorKey,
+      initialLocation: LoadingScreen.route,
+      routes: <RouteBase>[
+        ShellRoute(
+          navigatorKey: shellNavigatorKey,
+          builder: (BuildContext context, GoRouterState state, Widget child) {
+            return ScaffoldWithNavBar(
+              child: child,
+            );
+          },
+          routes: <RouteBase>[
+            GoRoute(
+              path: LoadingScreen.route,
+              builder: (BuildContext context, GoRouterState state) {
+                return const LoadingScreen();
+              },
+            ),
+            GoRoute(
+              path: WalletScreen.route,
+              builder: (BuildContext context, GoRouterState state) {
+                return const WalletScreen();
+              },
+              routes: <RouteBase>[
+                GoRoute(
+                  path: SendScreen.subRouteName,
+                  // Use root navigator so the screen overlays the application shell
+                  parentNavigatorKey: rootNavigatorKey,
+                  builder: (BuildContext context, GoRouterState state) {
+                    if (state.extra != null) {
+                      return SendScreen(encodedDestination: state.extra as String?);
+                    }
+                    return const SendScreen();
+                  },
+                ),
+                GoRoute(
+                  path: SeedScreen.subRouteName,
+                  // Use root navigator so the screen overlays the application shell
+                  parentNavigatorKey: rootNavigatorKey,
+                  builder: (BuildContext context, GoRouterState state) {
+                    return const SeedScreen();
+                  },
+                ),
+                GoRoute(
+                    path: OnboardingScreen.subRouteName,
+                    parentNavigatorKey: rootNavigatorKey,
+                    builder: (BuildContext context, GoRouterState state) {
+                      return const OnboardingScreen();
+                    }),
+                GoRoute(
+                  path: ReceiveScreen.subRouteName,
+                  // Use root navigator so the screen overlays the application shell
+                  parentNavigatorKey: rootNavigatorKey,
+                  builder: (BuildContext context, GoRouterState state) {
+                    if (state.extra != null) {
+                      return ReceiveScreen(walletType: state.extra as WalletType);
+                    }
+                    return const ReceiveScreen();
+                  },
+                ),
+                GoRoute(
+                  path: ScannerScreen.subRouteName,
+                  parentNavigatorKey: rootNavigatorKey,
+                  builder: (BuildContext context, GoRouterState state) {
+                    return const ScannerScreen();
+                  },
+                ),
+              ],
+            ),
+            GoRoute(
+                path: StableScreen.route,
+                builder: (BuildContext context, GoRouterState state) {
+                  return const StableScreen();
+                }),
+            GoRoute(
+              path: TradeScreen.route,
+              builder: (BuildContext context, GoRouterState state) {
+                return const TradeScreen();
+              },
+              routes: const [],
+            ),
+          ],
+        ),
+        GoRoute(
+            path: WelcomeScreen.route,
+            parentNavigatorKey: rootNavigatorKey,
+            builder: (BuildContext context, GoRouterState state) {
+              return const WelcomeScreen();
+            },
+            routes: const []),
+      ]);
+}
