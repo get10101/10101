6a5c7fc53fd23039f4e92238eefb2656ad0e5919
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-11-16 12:58:49 +1100

    refactor(app): Refactor recover rollover

--- mobile/native/src/ln_dlc/recover_rollover.rs
+++ mobile/native/src/ln_dlc/recover_rollover.rs
@@ -1,5 +1,6 @@
 use crate::ln_dlc::node::Node;
 use anyhow::Result;
+use bitcoin::hashes::hex::ToHex;
 use ln_dlc_node::node::rust_dlc_manager::channel::signed_channel::SignedChannelState;
 use ln_dlc_node::node::rust_dlc_manager::channel::Channel;
 use ln_dlc_node::node::rust_dlc_manager::Storage;
@@ -7,51 +8,65 @@ use ln_dlc_node::node::rust_dlc_manager::Storage;
 impl Node {
     /// Checks and recovers from a potential stuck rollover.
     pub async fn recover_rollover(&self) -> Result<()> {
-        tracing::debug!("Checking if dlc channel got stuck in rollover.");
+        tracing::debug!("Checking if DLC channel got stuck during rollover");
+
         let channels = self.inner.channel_manager.list_channels();
         let channel_details = match channels.first() {
             Some(channel_details) => channel_details,
             None => {
-                tracing::debug!("No channel found. All good!");
+                tracing::debug!("No need to recover rollover: no LN channel found");
                 return Ok(());
             }
         };
 
-        let dlc_channels = self.inner.dlc_manager.get_store().get_sub_channels()?;
-        let dlc_channel = dlc_channels
+        let subchannels = self.inner.dlc_manager.get_store().get_sub_channels()?;
+        let subchannel = match subchannels
             .iter()
-            .find(|dlc_channel| dlc_channel.channel_id == channel_details.channel_id);
-
-        let dlc_channel = match dlc_channel {
-            Some(dlc_channel) => dlc_channel,
+            .find(|dlc_channel| dlc_channel.channel_id == channel_details.channel_id)
+        {
+            Some(subchannel) => subchannel,
             None => {
-                tracing::debug!("No dlc channel found. All good!");
+                tracing::debug!("No need to recover rollover: no subchannel found");
                 return Ok(());
             }
         };
 
-        let dlc_channel_id = match dlc_channel.get_dlc_channel_id(0) {
+        let channel_id_hex = subchannel.channel_id.to_hex();
+
+        let dlc_channel_id = match subchannel.get_dlc_channel_id(0) {
             Some(dlc_channel_id) => dlc_channel_id,
             None => {
-                tracing::warn!(channel_id=%hex::encode(dlc_channel.channel_id), "Couldn't get a dlc channel id for a dlc channel");
+                tracing::debug!(
+                    channel_id=%channel_id_hex,
+                    "Cannot consider subchannel for rollover recovery without DLC channel ID"
+                );
                 return Ok(());
             }
         };
 
-        let channel = self
+        let dlc_channel_id_hex = dlc_channel_id.to_hex();
+
+        let dlc_channel = self
             .inner
             .dlc_manager
             .get_store()
             .get_channel(&dlc_channel_id)?;
 
-        let signed_channel = match channel {
+        let signed_channel = match dlc_channel {
             Some(Channel::Signed(signed_channel)) => signed_channel,
-            Some(channel) => {
-                tracing::warn!(dlc_channel_id=%hex::encode(dlc_channel_id), "Found channel in unexpected state. Expected: Signed, Found: {channel:?}");
+            Some(_) => {
+                tracing::debug!(
+                    channel_id=%channel_id_hex,
+                    "No need to recover rollover: DLC channel not signed"
+                );
                 return Ok(());
             }
             None => {
-                tracing::warn!(dlc_channel_id=%hex::encode(dlc_channel_id), "Couldn't find channel");
+                tracing::warn!(
+                    channel_id=%channel_id_hex,
+                    dlc_channel_id=%dlc_channel_id_hex,
+                    "Expected DLC channel associated with subchannel not found"
+                );
                 return Ok(());
             }
         };
@@ -62,13 +77,24 @@ impl Node {
             | SignedChannelState::RenewConfirmed { .. }
             | SignedChannelState::RenewFinalized { .. } => {
                 let state = ln_dlc_node::node::signed_channel_state_name(&signed_channel);
-                tracing::warn!(state, "Found signed channel contract in an intermediate state. Rolling back, expecting coordinator to retry rollover!");
+
+                tracing::warn!(
+                    state,
+                    "Found signed DLC channel in an intermediate rollover state. \
+                     Rolling back and expecting coordinator to retry rollover"
+                );
+
                 self.inner.rollback_channel(&signed_channel)?;
             }
             _ => {
-                tracing::debug!(signed_channel_state=%signed_channel.state, "Channel is not in an intermediate rollover state. All good.")
+                tracing::debug!(
+                    signed_channel_state=%signed_channel.state,
+                    "No need to recover rollover: DLC channel is not in an \
+                     intermediate rollover state"
+                )
             }
         }
+
         Ok(())
     }
 }
