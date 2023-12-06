ae451e85f01dbbc73f6b14cfb18ce19a6747140b
Richard Holzeis <richard@holzeis.me>
2023-12-06 15:32:06 +0100

    feat: Open scanner on send button

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+- Feat: Open scanner on send button
+
 ## [1.7.0] - 2023-12-07
 
 - Fix: Reduce WebSocket reconnect timeout to 200ms.
--- mobile/lib/features/wallet/send/send_screen.dart
+++ mobile/lib/features/wallet/send/send_screen.dart
@@ -12,19 +12,17 @@ import 'package:get_10101/features/wallet/application/wallet_service.dart';
 import 'package:get_10101/features/wallet/domain/destination.dart';
 import 'package:get_10101/features/wallet/domain/wallet_type.dart';
 import 'package:get_10101/features/wallet/send/confirm_payment_modal.dart';
-import 'package:get_10101/features/wallet/send/enter_destination_modal.dart';
 import 'package:get_10101/features/wallet/wallet_change_notifier.dart';
 import 'package:get_10101/features/wallet/wallet_screen.dart';
-import 'package:get_10101/logger/logger.dart';
 import 'package:provider/provider.dart';
 
 class SendScreen extends StatefulWidget {
   static const route = "${WalletScreen.route}/$subRouteName";
   static const subRouteName = "send";
 
-  final String? encodedDestination;
+  final Destination destination;
 
-  const SendScreen({super.key, this.encodedDestination});
+  const SendScreen({super.key, required this.destination});
 
   @override
   State<SendScreen> createState() => _SendScreenState();
@@ -33,11 +31,9 @@ class SendScreen extends StatefulWidget {
 class _SendScreenState extends State<SendScreen> {
   final _formKey = GlobalKey<FormState>();
   bool _valid = false;
-  bool _invalidDestination = false;
 
   ChannelInfo? channelInfo;
 
-  Destination? _destination;
   Amount _amount = Amount.zero();
 
   final TextEditingController _controller = TextEditingController();
@@ -58,27 +54,14 @@ class _SendScreenState extends State<SendScreen> {
 
   Future<void> init(ChannelInfoService channelInfoService, WalletService walletService) async {
     channelInfo = await channelInfoService.getChannelInfo();
-    if (widget.encodedDestination != null) {
-      final destination = await walletService.decodeDestination(widget.encodedDestination!);
-      setState(() {
-        if (destination != null) {
-          _destination = destination;
-          _amount = destination.amount;
-          _controller.text = _amount.formatted();
-
-          _invalidDestination = false;
-          _valid = _formKey.currentState?.validate() ?? false;
-        } else {
-          _invalidDestination = false;
-        }
-      });
-    }
+    setState(() {
+      _amount = widget.destination.amount;
+      _controller.text = _amount.formatted();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final WalletService walletService = context.watch<WalletChangeNotifier>().service;
-
     final balance = getBalance();
 
     return Scaffold(
@@ -91,49 +74,17 @@ class _SendScreenState extends State<SendScreen> {
             child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
               const Text("Destination", style: TextStyle(fontWeight: FontWeight.bold)),
               const SizedBox(height: 2),
-              OutlinedButton(
-                  onPressed: () => showEnterDestinationModal(context, (encodedDestination) {
-                        walletService.decodeDestination(encodedDestination).then((destination) {
-                          if (destination == null) {
-                            logger.w("Invalid destination!");
-                            setState(() => _invalidDestination = true);
-                            return;
-                          }
-
-                          setState(() {
-                            _destination = destination;
-                            _amount = destination.amount;
-                            _controller.text = _amount.formatted();
-
-                            _invalidDestination = false;
-                            _valid = _formKey.currentState?.validate() ?? false;
-                          });
-                        });
-                      }),
-                  style: OutlinedButton.styleFrom(
-                    side:
-                        BorderSide(color: _invalidDestination ? Colors.red[900]! : Colors.black87),
-                    minimumSize: const Size(20, 60),
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                          _destination?.raw != null
-                              ? truncateWithEllipsis(26, _destination!.raw)
-                              : "Set destination",
-                          style: const TextStyle(color: Colors.black87, fontSize: 16)),
-                      const Icon(Icons.edit, size: 20)
-                    ],
-                  )),
-              Visibility(
-                  visible: _invalidDestination,
-                  child: Padding(
-                      padding: const EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                      child: Text("Invalid destination",
-                          style: TextStyle(color: Colors.red[900], fontSize: 12)))),
+              InputDecorator(
+                decoration: InputDecoration(
+                  enabledBorder:
+                      const OutlineInputBorder(borderSide: BorderSide(color: Colors.black12)),
+                  labelStyle: const TextStyle(color: Colors.black87),
+                  filled: true,
+                  fillColor: Colors.grey[50],
+                ),
+                child: Text(truncateWithEllipsis(26, widget.destination.raw),
+                    style: const TextStyle(fontSize: 15)),
+              ),
               const SizedBox(height: 15),
               const Text("From", style: TextStyle(fontWeight: FontWeight.bold)),
               const SizedBox(height: 2),
@@ -141,44 +92,38 @@ class _SendScreenState extends State<SendScreen> {
                   onPressed: null,
                   style: OutlinedButton.styleFrom(
                     minimumSize: const Size(20, 60),
-                    backgroundColor: _destination != null ? Colors.white : Colors.white24,
+                    backgroundColor: Colors.white,
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   ),
-                  child: Visibility(
-                    visible: _destination != null,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        _destination != null
-                            ? Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                                Icon(
-                                    _destination!.getWalletType() == WalletType.lightning
-                                        ? Icons.bolt
-                                        : Icons.currency_bitcoin,
-                                    size: 30),
-                                const SizedBox(width: 5),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      _destination!.getWalletType() == WalletType.lightning
-                                          ? "Lightning"
-                                          : "On-chain",
-                                      style: const TextStyle(color: Colors.black87, fontSize: 16),
-                                    ),
-                                    Text(formatSats(balance[_destination!.getWalletType()]!.$1))
-                                  ],
-                                ),
-                              ])
-                            : Container(),
-                        const Icon(Icons.arrow_drop_down_sharp, size: 30)
-                      ],
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                        Icon(
+                            widget.destination.getWalletType() == WalletType.lightning
+                                ? Icons.bolt
+                                : Icons.currency_bitcoin,
+                            size: 30),
+                        const SizedBox(width: 5),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              widget.destination.getWalletType() == WalletType.lightning
+                                  ? "Lightning"
+                                  : "On-chain",
+                              style: const TextStyle(color: Colors.black87, fontSize: 16),
+                            ),
+                            Text(formatSats(balance[widget.destination.getWalletType()]!.$1))
+                          ],
+                        ),
+                      ]),
+                      const Icon(Icons.arrow_drop_down_sharp, size: 30)
+                    ],
                   )),
               const SizedBox(height: 20),
               Visibility(
-                visible:
-                    _destination != null && _destination!.getWalletType() == WalletType.onChain,
+                visible: widget.destination.getWalletType() == WalletType.onChain,
                 replacement: const Text(
                   "Amount in sats",
                   style: TextStyle(fontWeight: FontWeight.bold),
@@ -193,7 +138,7 @@ class _SendScreenState extends State<SendScreen> {
                 controller: _controller,
                 label: "",
                 value: _amount,
-                enabled: _destination != null && _destination!.amount.sats == 0,
+                enabled: widget.destination.amount.sats == 0,
                 onChanged: (value) {
                   setState(() {
                     _amount = Amount.parseAmount(value);
@@ -207,7 +152,8 @@ class _SendScreenState extends State<SendScreen> {
 
                   final amount = Amount.parseAmount(value);
 
-                  if (amount.sats <= 0 && _destination!.getWalletType() == WalletType.lightning) {
+                  if (amount.sats <= 0 &&
+                      widget.destination.getWalletType() == WalletType.lightning) {
                     return "Amount cannot be 0";
                   }
 
@@ -215,16 +161,12 @@ class _SendScreenState extends State<SendScreen> {
                     return "Amount cannot be negative";
                   }
 
-                  if (_destination == null) {
-                    return "Missing destination";
-                  }
-
-                  final bal = balance[_destination!.getWalletType()]!.$1;
+                  final bal = balance[widget.destination.getWalletType()]!.$1;
                   if (amount.sats > bal.sats) {
                     return "Not enough funds.";
                   }
 
-                  final usebal = balance[_destination!.getWalletType()]!.$2;
+                  final usebal = balance[widget.destination.getWalletType()]!.$2;
 
                   if (amount.sats > usebal.sats) {
                     return "Not enough funds. ${formatSats(bal.sub(usebal))} have to remain.";
@@ -244,8 +186,7 @@ class _SendScreenState extends State<SendScreen> {
                   filled: true,
                   fillColor: Colors.grey[50],
                 ),
-                child: Text(_destination != null ? _destination!.description : "",
-                    style: const TextStyle(fontSize: 15)),
+                child: Text(widget.destination.description, style: const TextStyle(fontSize: 15)),
               ),
               Expanded(
                 child: Column(
@@ -255,7 +196,7 @@ class _SendScreenState extends State<SendScreen> {
                     ElevatedButton(
                         onPressed: !_valid
                             ? null
-                            : () => showConfirmPaymentModal(context, _destination!, _amount),
+                            : () => showConfirmPaymentModal(context, widget.destination, _amount),
                         child: const Text("Next")),
                   ],
                 ),
