c562776bb15f50c1901db22f02712ebade91768c
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2023-12-12 17:54:45 +1100

    fix(coordinator): Keep message-forwarding task alive
    Before we would end up bubbling up the error on `pool.get()`, which
    would silently break out of the loop, so we would stop processin new
    `OrderbookMessage`s.

--- coordinator/src/message.rs
+++ coordinator/src/message.rs
@@ -2,6 +2,7 @@ use crate::db::user;
 use crate::notifications::FcmToken;
 use crate::notifications::Notification;
 use crate::notifications::NotificationKind;
+use anyhow::Context;
 use anyhow::Result;
 use bitcoin::secp256k1::PublicKey;
 use commons::Message;
@@ -16,8 +17,10 @@ use std::sync::Arc;
 use tokio::sync::broadcast;
 use tokio::sync::broadcast::error::RecvError;
 use tokio::sync::mpsc;
+use tokio::sync::mpsc::Sender;
+use tokio::task::spawn_blocking;
 
-/// This value is arbitrarily set to 100 and defines the message accepted in the message
+/// This value is arbitrarily set to 100 and defines theff message accepted in the message
 /// channel buffer.
 const NOTIFICATION_BUFFER_SIZE: usize = 100;
 
@@ -33,14 +36,14 @@ pub enum OrderbookMessage {
 #[derive(Clone)]
 pub struct NewUserMessage {
     pub new_user: PublicKey,
-    pub sender: mpsc::Sender<Message>,
+    pub sender: Sender<Message>,
 }
 
 pub fn spawn_delivering_messages_to_authenticated_users(
     pool: Pool<ConnectionManager<PgConnection>>,
-    notification_sender: mpsc::Sender<Notification>,
+    notification_sender: Sender<Notification>,
     tx_user_feed: broadcast::Sender<NewUserMessage>,
-) -> (RemoteHandle<Result<()>>, mpsc::Sender<OrderbookMessage>) {
+) -> (RemoteHandle<()>, Sender<OrderbookMessage>) {
     let (sender, mut receiver) = mpsc::channel::<OrderbookMessage>(NOTIFICATION_BUFFER_SIZE);
 
     let authenticated_users = Arc::new(RwLock::new(HashMap::new()));
@@ -57,12 +60,12 @@ pub fn spawn_delivering_messages_to_authenticated_users(
                             .insert(new_user_msg.new_user, new_user_msg.sender);
                     }
                     Err(RecvError::Closed) => {
-                        tracing::error!("New user message sender died! Channel closed.");
+                        tracing::error!("New user message sender died! Channel closed");
                         break;
                     }
-                    Err(RecvError::Lagged(skip)) => tracing::warn!(%skip,
-                        "Lagging behind on new user message."
-                    ),
+                    Err(RecvError::Lagged(skip)) => {
+                        tracing::warn!(%skip, "Lagging behind on new user message")
+                    }
                 }
             }
         }
@@ -71,49 +74,19 @@ pub fn spawn_delivering_messages_to_authenticated_users(
     let (fut, remote_handle) = {
         async move {
             while let Some(notification) = receiver.recv().await {
-                let mut conn = pool.get()?;
-                match notification {
-                    OrderbookMessage::TraderMessage { trader_id, message , notification} => {
-                        tracing::info!(%trader_id, "Sending trader message: {message:?}");
-
-                        let trader = authenticated_users.read().get(&trader_id).cloned();
-
-                        match trader {
-                            Some(sender) => {
-                                if let Err(e) = sender.send(message).await {
-                                    tracing::warn!(%trader_id, "Connection lost to trader {e:#}");
-                                } else {
-                                    tracing::trace!(%trader_id, "Skipping optional push notifications as the user was successfully notified via the websocket.");
-                                    continue;
-                                }
-                            }
-                            None => tracing::warn!(%trader_id, "Trader is not connected."),
-                        };
-
-                        if let (Some(notification_kind),Some(user)) = (notification, user::by_id(&mut conn, trader_id.to_string())?) {
-                            tracing::debug!(%trader_id, "Sending push notification to user");
-
-                            match FcmToken::new(user.fcm_token) {
-                                Ok(fcm_token) => {
-                                    if let Err(e) = notification_sender
-                                        .send(Notification {
-                                            user_fcm_token: fcm_token,
-                                            notification_kind,
-                                        })
-                                        .await {
-                                        tracing::error!(%trader_id, "Failed to send push notification. Error: {e:#}");
-                                    }
-                                }
-                                Err(error) => {
-                                    tracing::error!(%trader_id, "Could not send notification to user. Error: {error:#}");
-                                }
-                            }
-                        }
-                    }
+                if let Err(e) = process_orderbook_message(
+                    pool.clone(),
+                    &authenticated_users,
+                    &notification_sender,
+                    notification,
+                )
+                .await
+                {
+                    tracing::error!("Failed to process orderbook message: {e:#}");
                 }
             }
 
-            Ok(())
+            tracing::error!("Channel closed");
         }
         .remote_handle()
     };
@@ -121,4 +94,64 @@ pub fn spawn_delivering_messages_to_authenticated_users(
     tokio::spawn(fut);
 
     (remote_handle, sender)
+}
+
+async fn process_orderbook_message(
+    pool: Pool<ConnectionManager<PgConnection>>,
+    authenticated_users: &RwLock<HashMap<PublicKey, Sender<Message>>>,
+    notification_sender: &Sender<Notification>,
+    notification: OrderbookMessage,
+) -> Result<()> {
+    let mut conn = spawn_blocking(move || pool.get())
+        .await
+        .expect("task to complete")?;
+
+    match notification {
+        OrderbookMessage::TraderMessage {
+            trader_id,
+            message,
+            notification,
+        } => {
+            tracing::info!(%trader_id, ?message, "Sending trader message");
+
+            let trader = authenticated_users.read().get(&trader_id).cloned();
+
+            match trader {
+                Some(sender) => {
+                    if let Err(e) = sender.send(message).await {
+                        tracing::warn!(%trader_id, "Connection lost to trader: {e:#}");
+                    } else {
+                        tracing::trace!(
+                            %trader_id,
+                            "Skipping optional push notifications as the user was successfully \
+                             notified via the websocket"
+                        );
+                        return Ok(());
+                    }
+                }
+                None => tracing::warn!(%trader_id, "Trader is not connected"),
+            };
+
+            let user = user::by_id(&mut conn, trader_id.to_string())
+                .context("Failed to get user by ID")?;
+
+            if let (Some(notification_kind), Some(user)) = (notification, user) {
+                tracing::debug!(%trader_id, "Sending push notification to user");
+
+                let fcm_token = FcmToken::new(user.fcm_token)?;
+
+                notification_sender
+                    .send(Notification {
+                        user_fcm_token: fcm_token,
+                        notification_kind,
+                    })
+                    .await
+                    .with_context(|| {
+                        format!("Failed to send push notification to trader {trader_id}")
+                    })?;
+            }
+        }
+    }
+
+    Ok(())
 }
