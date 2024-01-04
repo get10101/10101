f3e520dd75914773c3bab81b685c45b0ddfea20e
Philipp Hoenisch <philipp@coblox.tech>
2024-01-04 13:38:11 +0100

    chore: remove outdated tests:

--- crates/ln-dlc-node/src/tests/dlc/mod.rs
+++
@@ -1,4 +0,0 @@
-mod collaborative_settlement;
-mod create;
-mod dlc_setup_with_reconnects;
-mod non_collaborative_settlement;
--- crates/ln-dlc-node/src/tests/mod.rs
+++ crates/ln-dlc-node/src/tests/mod.rs
@@ -1,11 +1,8 @@
-use crate::channel::UserChannelId;
 use crate::config::app_config;
 use crate::config::coordinator_config;
-use crate::ln::calculate_channel_value;
 use crate::node::peer_manager::alias_as_bytes;
 use crate::node::GossipSourceConfig;
 use crate::node::InMemoryStore;
-use crate::node::LiquidityRequest;
 use crate::node::LnDlcNodeSettings;
 use crate::node::Node;
 use crate::node::NodeInfo;
@@ -41,14 +38,11 @@ use dlc_manager::subchannel::SubChannelState;
 use dlc_manager::Storage;
 use futures::Future;
 use lightning::events::Event;
-use lightning::ln::channelmanager::ChannelDetails;
 use lightning::util::config::UserConfig;
 use rand::distributions::Alphanumeric;
 use rand::thread_rng;
 use rand::Rng;
 use rand::RngCore;
-use rust_decimal::prelude::ToPrimitive;
-use rust_decimal::Decimal;
 use std::env::temp_dir;
 use std::net::TcpListener;
 use std::path::PathBuf;
@@ -61,12 +55,7 @@ use tokio::sync::watch;
 use tokio::task::block_in_place;
 
 mod bitcoind;
-mod dlc;
 mod dlc_channel;
-mod just_in_time_channel;
-mod multi_hop_payment;
-mod probe;
-mod single_hop_payment;
 
 #[cfg(feature = "load_tests")]
 mod load;
@@ -96,6 +85,37 @@ fn init_tracing() {
             .with_test_writer()
             .init()
     })
+}
+
+#[cfg(test)]
+async fn wait_until_sub_channel_state(
+    timeout: Duration,
+    node: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
+    counterparty_pk: PublicKey,
+    target_state: SubChannelStateName,
+) -> Result<SubChannel> {
+    wait_until(timeout, || async {
+        node.process_incoming_messages()?;
+
+        let dlc_channels = node.dlc_manager.get_store().get_sub_channels()?;
+
+        Ok(dlc_channels
+            .iter()
+            .find(|channel| {
+                let current_state = SubChannelStateName::from(&channel.state);
+
+                tracing::info!(
+                    node_id = %node.info.pubkey,
+                    target = ?target_state,
+                    current = ?current_state,
+                    "Waiting for DLC subchannel to reach state"
+                );
+
+                channel.counter_party == counterparty_pk && current_state == target_state
+            })
+            .cloned())
+    })
+    .await
 }
 
 impl Node<TenTenOneInMemoryStorage, InMemoryStore> {
@@ -266,90 +286,6 @@ impl Node<TenTenOneInMemoryStorage, InMemoryStore> {
         Ok(balance.confirmed)
     }
 
-    /// Initiates the opening of a private channel _and_ waits for the channel to be usable.
-    async fn open_private_channel(
-        &self,
-        peer: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-        amount_us: u64,
-        amount_them: u64,
-    ) -> Result<ChannelDetails> {
-        self.open_channel(peer, amount_us, amount_them, false).await
-    }
-
-    /// Initiates the opening of a public channel _and_ waits for the channel to be usable.
-    async fn open_public_channel(
-        &self,
-        peer: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-        amount_us: u64,
-        amount_them: u64,
-    ) -> Result<ChannelDetails> {
-        self.open_channel(peer, amount_us, amount_them, true).await
-    }
-
-    /// Initiates the opening of a channel _and_ waits for the channel to be usable.
-    async fn open_channel(
-        &self,
-        peer: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-        amount_us: u64,
-        amount_them: u64,
-        is_public: bool,
-    ) -> Result<ChannelDetails> {
-        let temp_channel_id = self.initiate_open_channel(
-            peer.info.pubkey,
-            amount_us + amount_them,
-            amount_them,
-            is_public,
-        )?;
-
-        let (does_manually_accept_inbound_channels, required_confirmations) =
-            block_in_place(|| {
-                let config = peer.ldk_config.read();
-
-                (
-                    config.manually_accept_inbound_channels,
-                    config.channel_handshake_config.minimum_depth,
-                )
-            });
-
-        // The config flag `channel_config.manually_accept_inbound_channels` implies that the peer
-        // will accept 0-conf channels
-        if !does_manually_accept_inbound_channels {
-            bitcoind::mine(required_confirmations as u16).await?;
-        }
-
-        let channel_details = tokio::time::timeout(Duration::from_secs(30), async {
-            loop {
-                if let Some(details) = self
-                    .channel_manager
-                    .list_usable_channels()
-                    .iter()
-                    .find(|c| c.counterparty.node_id == peer.info.pubkey)
-                {
-                    break details.clone();
-                }
-
-                // Only sync if 0-conf channels are disabled
-                if !does_manually_accept_inbound_channels {
-                    // We need to sync both parties, even if
-                    // `trust_own_funding_0conf` is true for the creator
-                    // of the channel (`self`)
-                    self.sync_wallets().await.unwrap();
-                    peer.sync_wallets().await.unwrap();
-                }
-
-                tracing::debug!(
-                    peer = %peer.info,
-                    temp_channel_id = %hex::encode(temp_channel_id.0),
-                    "Waiting for channel to be usable"
-                );
-                tokio::time::sleep(Duration::from_millis(100)).await;
-            }
-        })
-        .await?;
-
-        Ok(channel_details)
-    }
-
     pub fn disconnect(&self, peer: NodeInfo) {
         self.peer_manager.disconnect_by_node_id(peer.pubkey)
     }
@@ -370,69 +306,6 @@ impl Node<TenTenOneInMemoryStorage, InMemoryStore> {
             self.announcement_addresses.clone(),
         );
     }
-}
-
-async fn setup_coordinator_payer_channel(
-    coordinator: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-    payer: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-    payee_pk: PublicKey,
-    // How much the _payee_ should have in the coordinator-payee channel. This value corresponds to
-    // the value in the JIT channel-opening invoice, minus the channel-opening fee.
-    coordinator_payee_inbound_liquity_sat: u64,
-) -> (u64, LiquidityRequest) {
-    // The `LiquidityRequest` associated with the JIT channel being opened.
-    let liquidity_request = LiquidityRequest {
-        user_channel_id: UserChannelId::new(),
-        liquidity_option_id: 1,
-        trader_id: payee_pk,
-        // Trade up to the full payee balance in the JIT channel.
-        trade_up_to_sats: coordinator_payee_inbound_liquity_sat,
-        max_deposit_sats: coordinator_payee_inbound_liquity_sat * 2,
-        coordinator_leverage: 1.0,
-        fee_sats: 10_000,
-    };
-
-    let expected_coordinator_payee_channel_value = calculate_channel_value(
-        coordinator_payee_inbound_liquity_sat * 1_000,
-        &liquidity_request,
-    );
-
-    // How much the _payer_ should have in the payer-coordinator channel. Since this channel is only
-    // used to fund the payee, we just need to fund it with enough coins for that. WE double this
-    // amount to be on the safe side.
-    let payer_coordinator_outbound_liquidity_sat =
-        (coordinator_payee_inbound_liquity_sat + liquidity_request.fee_sats) * 2;
-    // How much the _coordinator_ should have in the payer-coordinator channel. This value does not
-    // matter too much as the coordinator is just receiving in this channel. To avoid complications
-    // we just set it to the payer's outbound liquidity, so that the channel is balanced.
-    let payer_coordinator_inbound_liquidity_sat = payer_coordinator_outbound_liquidity_sat;
-
-    // The coordinator will have to (1) open a channel with the payer, (2) push some sats to the
-    // payer in that channel, (3) open a JIT channel with the payee with a certain amount of
-    // coordinator outbound liquidity based on the `LiquidityRequest`. The coordinator will need
-    // sufficient in the on-chain wallet to be able to open these two channels.
-    let coordinator_fund_amount = payer_coordinator_inbound_liquidity_sat
-        + payer_coordinator_outbound_liquidity_sat
-        + expected_coordinator_payee_channel_value;
-
-    // We double this computed amount to be on the safe side.
-    let coordinator_fund_amount = coordinator_fund_amount * 2;
-
-    coordinator
-        .fund(Amount::from_sat(coordinator_fund_amount))
-        .await
-        .unwrap();
-
-    coordinator
-        .open_private_channel(
-            payer,
-            payer_coordinator_inbound_liquidity_sat,
-            payer_coordinator_outbound_liquidity_sat,
-        )
-        .await
-        .unwrap();
-
-    (expected_coordinator_payee_channel_value, liquidity_request)
 }
 
 fn random_tmp_dir() -> PathBuf {
@@ -486,23 +359,6 @@ fn log_channel_id(node: &Node<TenTenOneInMemoryStorage, InMemoryStore>, index: u
     );
 }
 
-async fn wait_for_n_usable_channels(
-    channel_count: usize,
-    node: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-) -> Result<()> {
-    wait_until(Duration::from_secs(20), || async {
-        let usable_channels_length = node.channel_manager.list_usable_channels().len();
-        let scids_set = node
-            .channel_manager
-            .list_usable_channels()
-            .iter()
-            .all(|c| c.get_inbound_payment_scid().is_some());
-
-        Ok((usable_channels_length == channel_count && scids_set).then_some(()))
-    })
-    .await
-}
-
 async fn wait_until<P, T, F>(timeout: Duration, predicate_fn: P) -> Result<T>
 where
     P: Fn() -> F,
@@ -517,55 +373,6 @@ where
         }
     })
     .await?
-}
-
-async fn wait_until_sub_channel_state(
-    timeout: Duration,
-    node: &Node<TenTenOneInMemoryStorage, InMemoryStore>,
-    counterparty_pk: PublicKey,
-    target_state: SubChannelStateName,
-) -> Result<SubChannel> {
-    wait_until(timeout, || async {
-        node.process_incoming_messages()?;
-
-        let dlc_channels = node.dlc_manager.get_store().get_sub_channels()?;
-
-        Ok(dlc_channels
-            .iter()
-            .find(|channel| {
-                let current_state = SubChannelStateName::from(&channel.state);
-
-                tracing::info!(
-                    node_id = %node.info.pubkey,
-                    target = ?target_state,
-                    current = ?current_state,
-                    "Waiting for DLC subchannel to reach state"
-                );
-
-                channel.counter_party == counterparty_pk && current_state == target_state
-            })
-            .cloned())
-    })
-    .await
-}
-
-/// Calculate the fee paid to route a payment through a node, in msat. That is, the difference
-/// between the inbound HTLC and the outbound HTLC.
-///
-/// The `channel_config` is that of the routing node.
-fn calculate_routing_fee_msat(
-    channel_config: lightning::util::config::ChannelConfig,
-    invoice_amount_sat: u64,
-) -> u64 {
-    let flat_fee_msat = Decimal::from(channel_config.forwarding_fee_base_msat);
-    let forwarding_fee_millionths_of_a_sat_per_sat =
-        Decimal::from(channel_config.forwarding_fee_proportional_millionths);
-
-    let proportional_fee_msat_per_sat =
-        forwarding_fee_millionths_of_a_sat_per_sat / Decimal::ONE_THOUSAND;
-    let proportional_fee_msat = Decimal::from(invoice_amount_sat) * proportional_fee_msat_per_sat;
-
-    (flat_fee_msat + proportional_fee_msat).to_u64().unwrap()
 }
 
 #[derive(PartialEq, Debug)]
