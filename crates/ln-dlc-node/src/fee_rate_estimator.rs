68d532c0015ae2b04dd5fd801412f9bae6a51846
Philipp Hoenisch <philipp@coblox.tech>
2024-01-29 09:15:00 +0100

    feat: replace fee rate estimator with mempool.space client
    Signed-off-by: Philipp Hoenisch <philipp@coblox.tech>

--- crates/ln-dlc-node/src/fee_rate_estimator.rs
+++ crates/ln-dlc-node/src/fee_rate_estimator.rs
@@ -1,34 +1,25 @@
 use anyhow::Result;
 use bdk::FeeRate;
+use bitcoin::Network;
 use lightning::chain::chaininterface::ConfirmationTarget;
 use lightning::chain::chaininterface::FeeEstimator;
 use lightning::chain::chaininterface::FEERATE_FLOOR_SATS_PER_KW;
 use parking_lot::RwLock;
 use std::collections::HashMap;
 
-const CONFIRMATION_TARGETS: [(ConfirmationTarget, usize); 4] = [
-    // We choose an extremely high background confirmation target to avoid force-closing channels
-    // unnecessarily.
-    (ConfirmationTarget::Background, 1008),
-    // We just want to end up in the mempool eventually.  We just set the target to 1008
-    // as that is esplora's highest block target available
-    (ConfirmationTarget::MempoolMinimum, 1008),
-    (ConfirmationTarget::Normal, 6),
-    (ConfirmationTarget::HighPriority, 3),
-];
-
 /// Default values used when constructing the [`FeeRateEstimator`] if the fee rate sever cannot give
 /// us up-to-date values.
 ///
 /// In sats/kwu.
-const FEE_RATE_DEFAULTS: [(ConfirmationTarget, u32); 3] = [
-    (ConfirmationTarget::Background, FEERATE_FLOOR_SATS_PER_KW),
-    (ConfirmationTarget::Normal, 2000),
-    (ConfirmationTarget::HighPriority, 5000),
+const FEE_RATE_DEFAULTS: [(ConfirmationTarget, u32); 4] = [
+    (ConfirmationTarget::MempoolMinimum, 1000),
+    (ConfirmationTarget::Background, 2000),
+    (ConfirmationTarget::Normal, 3000),
+    (ConfirmationTarget::HighPriority, 4000),
 ];
 
 pub struct FeeRateEstimator {
-    client: esplora_client::BlockingClient,
+    client: mempool::MempoolFeeRateEstimator,
     fee_rate_cache: RwLock<HashMap<ConfirmationTarget, FeeRate>>,
 }
 
@@ -42,31 +33,27 @@ impl EstimateFeeRate for FeeRateEstimator {
     }
 }
 
+fn to_mempool_network(value: Network) -> mempool::Network {
+    match value {
+        Network::Bitcoin => mempool::Network::Mainnet,
+        Network::Testnet => mempool::Network::Testnet,
+        Network::Signet => mempool::Network::Signet,
+        Network::Regtest => mempool::Network::Local,
+    }
+}
+
 impl FeeRateEstimator {
     /// Constructor for the [`FeeRateEstimator`].
-    pub fn new(esplora_url: String) -> Self {
-        let client = esplora_client::BlockingClient::from_agent(esplora_url, ureq::agent());
+    pub fn new(network: Network) -> Self {
+        let client = mempool::MempoolFeeRateEstimator::new(to_mempool_network(network));
 
-        let initial_fee_rates = match client.get_fee_estimates() {
-            Ok(estimates) => {
-                HashMap::from_iter(CONFIRMATION_TARGETS.into_iter().map(|(target, n_blocks)| {
-                    let fee_rate = esplora_client::convert_fee_rate(n_blocks, estimates.clone())
-                        .expect("fee rates for our confirmation targets");
-                    let fee_rate = FeeRate::from_sat_per_vb(fee_rate);
+        tracing::warn!(defaults = ?FEE_RATE_DEFAULTS, "Initializing fee rate cache with default values.");
 
-                    (target, fee_rate)
-                }))
-            }
-            Err(e) => {
-                tracing::warn!(defaults = ?FEE_RATE_DEFAULTS, "Initializing fee rate cache with default values: {e:#}");
-
-                HashMap::from_iter(
-                    FEE_RATE_DEFAULTS.into_iter().map(|(target, fee_rate)| {
-                        (target, FeeRate::from_sat_per_kwu(fee_rate as f32))
-                    }),
-                )
-            }
-        };
+        let initial_fee_rates = HashMap::from_iter(
+            FEE_RATE_DEFAULTS
+                .into_iter()
+                .map(|(target, fee_rate)| (target, FeeRate::from_sat_per_kwu(fee_rate as f32))),
+        );
 
         let fee_rate_cache = RwLock::new(initial_fee_rates);
 
@@ -85,21 +72,29 @@ impl FeeRateEstimator {
     }
 
     pub(crate) async fn update(&self) -> Result<()> {
-        let estimates = self.client.get_fee_estimates()?;
+        let estimates = self.client.fetch_fee().await?;
 
         let mut locked_fee_rate_cache = self.fee_rate_cache.write();
-        for (target, n_blocks) in CONFIRMATION_TARGETS {
-            let fee_rate = esplora_client::convert_fee_rate(n_blocks, estimates.clone())?;
 
-            let fee_rate = FeeRate::from_sat_per_vb(fee_rate);
+        locked_fee_rate_cache.insert(
+            ConfirmationTarget::MempoolMinimum,
+            FeeRate::from_sat_per_vb(estimates.minimum_fee as f32),
+        );
 
-            locked_fee_rate_cache.insert(target, fee_rate);
-            tracing::trace!(
-                n_blocks_confirmation = %n_blocks,
-                sats_per_kwu = %fee_rate.fee_wu(1000),
-                "Updated fee rate estimate",
-            );
-        }
+        locked_fee_rate_cache.insert(
+            ConfirmationTarget::Background,
+            FeeRate::from_sat_per_vb(estimates.economy_fee as f32),
+        );
+
+        locked_fee_rate_cache.insert(
+            ConfirmationTarget::Normal,
+            FeeRate::from_sat_per_vb(estimates.hour_fee as f32),
+        );
+
+        locked_fee_rate_cache.insert(
+            ConfirmationTarget::HighPriority,
+            FeeRate::from_sat_per_vb(estimates.fastest_fee as f32),
+        );
 
         Ok(())
     }
