dd97985822f6f2b0cea630608ee6e5eeea34037d
Lucas Soriano del Pino <lucas_soriano@fastmail.com>
2024-02-28 18:39:02 +1100

    fix(app): Show log file location on desktop
    The share button wasn't working on desktop anyway.

--- mobile/lib/common/settings/app_info_screen.dart
+++ mobile/lib/common/settings/app_info_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:get_10101/common/color.dart';
+import 'package:get_10101/common/more_info.dart';
 import 'package:get_10101/common/settings/open_telegram.dart';
 import 'package:get_10101/common/settings/settings_screen.dart';
 import 'package:get_10101/common/snack_bar.dart';
 import 'package:get_10101/logger/logger.dart';
-
 import 'package:go_router/go_router.dart';
-
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:get_10101/ffi.dart' as rust;
 import 'package:url_launcher/url_launcher.dart';
@@ -229,56 +226,4 @@ class _AppInfoScreenState extends State<AppInfoScreen> {
           )),
     );
   }
-}
-
-Widget moreInfo(BuildContext context,
-    {required String title, required String info, bool showCopyButton = false}) {
-  return Container(
-    padding: const EdgeInsets.all(15),
-    child: Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style:
-                  const TextStyle(fontSize: 17, fontWeight: FontWeight.w400, color: Colors.black),
-            ),
-            const SizedBox(
-              height: 7,
-            ),
-            showCopyButton
-                ? SizedBox(
-                    width: MediaQuery.of(context).size.width - 100,
-                    child: Text(
-                      info,
-                      style: TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.w300, color: Colors.grey.shade700),
-                    ))
-                : const SizedBox()
-          ],
-        ),
-        showCopyButton
-            ? GestureDetector(
-                onTap: () async {
-                  showSnackBar(ScaffoldMessenger.of(context), "Copied $info");
-                  await Clipboard.setData(ClipboardData(text: info));
-                },
-                child: Icon(
-                  Icons.copy,
-                  size: 17,
-                  color: tenTenOnePurple.shade800,
-                ),
-              )
-            : Text(
-                info,
-                style: TextStyle(
-                    fontSize: 18, fontWeight: FontWeight.w300, color: Colors.grey.shade700),
-              )
-      ],
-    ),
-  );
 }
