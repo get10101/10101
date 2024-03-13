5315759a937fbc60d835df42dc91d7544a5c03a3
Richard Holzeis <richard@holzeis.me>
2024-03-13 16:24:49 +0100

    chore(app): Fix share logs button styling

--- mobile/lib/common/settings/share_logs_screen.dart
+++ mobile/lib/common/settings/share_logs_screen.dart
@@ -76,75 +76,64 @@ class _ShareLogsScreenState extends State<ShareLogsScreen> {
                 margin: const EdgeInsets.all(10),
                 child: Column(
                   children: [
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    const SizedBox(height: 20),
                     const Text(
-                      "You can  either  export your application logs or save them for future reference.",
+                      "You can either export your application logs or save them for future reference.",
                       style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        FutureBuilder(
-                            future: HybridOutput.logFilePath(),
-                            builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
-                              switch (defaultTargetPlatform) {
-                                case TargetPlatform.android:
-                                case TargetPlatform.iOS:
-                                  return GestureDetector(
-                                    onTap: () async {
-                                      logger.toString();
+                    const SizedBox(height: 20),
+                    FutureBuilder(
+                        future: HybridOutput.logFilePath(),
+                        builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+                          switch (defaultTargetPlatform) {
+                            case TargetPlatform.android:
+                            case TargetPlatform.iOS:
+                              return GestureDetector(
+                                onTap: () async {
+                                  logger.toString();
 
-                                      var logsAsString = await snapshot.data!.readAsString();
-                                      final List<int> bytes = utf8.encode(logsAsString);
-                                      final Directory tempDir = await getTemporaryDirectory();
-                                      String now =
-                                          DateFormat('yyyy-MM-dd_HHmmss').format(DateTime.now());
-                                      final String filePath = '${tempDir.path}/$now.log';
-                                      await File(filePath).writeAsBytes(bytes);
-                                      final XFile logFile = XFile(filePath);
-                                      Share.shareXFiles([logFile], text: 'Logs from $now');
-                                    },
-                                    child: Container(
-                                      padding:
-                                          const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                                      decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          borderRadius: BorderRadius.circular(15)),
-                                      child: Row(
-                                        children: [
-                                          Icon(
-                                            Icons.ios_share_outlined,
-                                            color: tenTenOnePurple.shade800,
-                                            size: 22,
-                                          ),
-                                          const SizedBox(
-                                            width: 20,
-                                          ),
-                                          Text(
-                                            "Share Logs",
-                                            style: TextStyle(
-                                                color: tenTenOnePurple.shade800,
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.w500),
-                                          )
-                                        ],
+                                  var logsAsString = await snapshot.data!.readAsString();
+                                  final List<int> bytes = utf8.encode(logsAsString);
+                                  final Directory tempDir = await getTemporaryDirectory();
+                                  String now =
+                                      DateFormat('yyyy-MM-dd_HHmmss').format(DateTime.now());
+                                  final String filePath = '${tempDir.path}/$now.log';
+                                  await File(filePath).writeAsBytes(bytes);
+                                  final XFile logFile = XFile(filePath);
+                                  Share.shareXFiles([logFile], text: 'Logs from $now');
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white, borderRadius: BorderRadius.circular(15)),
+                                  child: Row(
+                                    children: [
+                                      Icon(
+                                        Icons.ios_share_outlined,
+                                        color: tenTenOnePurple.shade800,
+                                        size: 22,
                                       ),
-                                    ),
-                                  );
-                                default:
-                                  return moreInfo(context,
-                                      title: "Log file location",
-                                      info: snapshot.data!.path,
-                                      showCopyButton: true);
-                              }
-                            }),
-                      ],
-                    ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(
+                                        "Share Logs",
+                                        style: TextStyle(
+                                            color: tenTenOnePurple.shade800,
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              );
+                            default:
+                              return moreInfo(context,
+                                  title: "Log file location",
+                                  info: snapshot.data!.path,
+                                  showCopyButton: true);
+                          }
+                        }),
                     const SizedBox(height: 20),
                     Container(
                       padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
