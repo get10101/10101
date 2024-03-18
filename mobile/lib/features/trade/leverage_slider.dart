095dd6e15802acc96f4282ae6f6ab361e5444767
Philipp Hoenisch <philipp@coblox.tech>
2024-03-18 10:46:45 +1000

    chore: replace leverage slider with SfSlider

--- mobile/lib/features/trade/leverage_slider.dart
+++ mobile/lib/features/trade/leverage_slider.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get_10101/common/color.dart';
 import 'package:get_10101/features/trade/trade_theme.dart';
-
-const gradientColors = <Color>[Colors.green, Colors.deepOrange];
-
-const LinearGradient gradient = LinearGradient(colors: gradientColors);
+import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_sliders/sliders.dart';
+import 'package:syncfusion_flutter_core/theme.dart' as slider_theme;
 
 const double minLeverage = 1.0;
 const double maxLeverage = 5.0;
@@ -12,11 +13,11 @@ const double maxLeverage = 5.0;
 /// It uses linear scale and fractional leverage values are rounded to the nearest integer.
 class LeverageSlider extends StatefulWidget {
   final double initialValue;
-  final bool isActive;
   final Function(double) onLeverageChanged;
+  final bool isActive;
 
   const LeverageSlider(
-      {required this.onLeverageChanged, this.initialValue = 2, this.isActive = true, super.key});
+      {required this.onLeverageChanged, this.initialValue = 2, super.key, required this.isActive});
 
   @override
   State<LeverageSlider> createState() => _LeverageSliderState();
@@ -24,73 +25,85 @@ class LeverageSlider extends StatefulWidget {
 
 class _LeverageSliderState extends State<LeverageSlider> {
   late double _leverage;
-  late bool isActive;
 
   @override
   void initState() {
     _leverage = widget.initialValue;
-    isActive = widget.isActive;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    TradeTheme tradeTheme = Theme.of(context).extension<TradeTheme>()!;
-
-    return AbsorbPointer(
-        // We want to deactivate the leverage slider when creating an order
-        // whilst we have an open position.
-        absorbing: !isActive,
-        child: Opacity(
-            opacity: isActive ? 1.0 : 0.5,
-            child: Row(
-              children: [
-                LeverageButton(
-                    label: "-",
-                    onPressed: () {
-                      if (_leverage > minLeverage) {
-                        updateLeverage(--_leverage);
-                      }
-                    }),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Leverage: x${_leverage.round()}",
-                        style: TextStyle(color: colorFromLeverage()),
-                      ),
-                      SliderTheme(
-                        data: SliderTheme.of(context).copyWith(
-                          thumbColor: colorFromLeverage(),
-                          trackShape: const GradientRectSliderTrackShape(),
-                          overlayShape: SliderComponentShape.noOverlay,
-                          inactiveTrackColor:
-                              tradeTheme.leverageInactiveSliderTrackColor.withOpacity(0.2),
-                          inactiveTickMarkColor: tradeTheme.leverageInactiveTicksColor,
-                        ),
-                        child: Slider(
-                          value: _leverage,
-                          min: 1,
-                          max: maxLeverage,
-                          divisions: (maxLeverage - 1).toInt(),
-                          label: "x${_leverage.round().toString()}",
-                          onChanged: (double value) {
-                            updateLeverage(value);
-                          },
-                        ),
-                      )
-                    ],
+    return InputDecorator(
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        labelText: "Leverage",
+        labelStyle: const TextStyle(color: tenTenOnePurple),
+        filled: true,
+        fillColor: Colors.white,
+        errorStyle: TextStyle(
+          color: Colors.red[900],
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: SizedBox(
+          height: 35,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RoundedIconButton(
+                icon: FontAwesomeIcons.minus,
+                onTap: () {
+                  setState(() {
+                    updateLeverage(_leverage > 1 ? _leverage - 1.0 : _leverage);
+                  });
+                },
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 2, right: 2),
+                  child: slider_theme.SfSliderTheme(
+                    data: slider_theme.SfSliderThemeData(
+                      activeLabelStyle: const TextStyle(color: Colors.black, fontSize: 12),
+                      inactiveLabelStyle: const TextStyle(color: Colors.black45, fontSize: 12),
+                      activeTrackColor: tenTenOnePurple.shade50,
+                      inactiveTrackColor: tenTenOnePurple.shade50,
+                    ),
+                    child: SfSlider(
+                      min: 1,
+                      max: maxLeverage,
+                      value: _leverage,
+                      stepSize: 1,
+                      interval: 1,
+                      showTicks: true,
+                      showLabels: true,
+                      enableTooltip: true,
+                      tooltipShape: const SfPaddleTooltipShape(),
+                      numberFormat: NumberFormat("x"),
+                      tooltipTextFormatterCallback: (dynamic actualValue, String formattedText) {
+                        return "${actualValue}x";
+                      },
+                      onChanged: widget.isActive
+                          ? (dynamic value) {
+                              updateLeverage(value);
+                            }
+                          : null,
+                    ),
                   ),
                 ),
-                LeverageButton(
-                    label: "+",
-                    onPressed: () {
-                      if (_leverage < maxLeverage) {
-                        updateLeverage(++_leverage);
-                      }
-                    })
-              ],
-            )));
+              ),
+              RoundedIconButton(
+                icon: FontAwesomeIcons.plus,
+                onTap: () {
+                  updateLeverage(_leverage < maxLeverage ? _leverage + 1.0 : maxLeverage);
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   updateLeverage(double leverage) {
@@ -99,25 +112,6 @@ class _LeverageSliderState extends State<LeverageSlider> {
     });
 
     widget.onLeverageChanged(_leverage);
-  }
-
-  Color colorFromLeverage() {
-    return lerpGradient(gradientColors, <double>[0, 100], _leverage * 10);
-  }
-
-  Color lerpGradient(List<Color> colors, List<double> stops, double t) {
-    for (var s = 0; s < stops.length - 1; s++) {
-      final leftStop = stops[s], rightStop = stops[s + 1];
-      final leftColor = colors[s], rightColor = colors[s + 1];
-      if (t <= leftStop) {
-        return leftColor;
-      } else if (t < rightStop) {
-        final sectionT = (t - leftStop) / (rightStop - leftStop);
-        return Color.lerp(leftColor, rightColor, sectionT) ?? Colors.white;
-      }
-    }
-
-    return colors.last;
   }
 }
 
@@ -148,134 +142,34 @@ class LeverageButton extends StatelessWidget {
   }
 }
 
-/// Code copied from `RoundedRectSliderTrackShape` and modified to use a Gradiant
-class GradientRectSliderTrackShape extends SliderTrackShape with BaseSliderTrackShape {
-  const GradientRectSliderTrackShape();
+class RoundedIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onTap;
+
+  const RoundedIconButton({
+    Key? key,
+    required this.icon,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
-  void paint(
-    PaintingContext context,
-    Offset offset, {
-    required RenderBox parentBox,
-    required SliderThemeData sliderTheme,
-    required Animation<double> enableAnimation,
-    required TextDirection textDirection,
-    required Offset thumbCenter,
-    Offset? secondaryOffset,
-    bool isDiscrete = false,
-    bool isEnabled = false,
-    double additionalActiveTrackHeight = 2,
-  }) {
-    assert(sliderTheme.disabledActiveTrackColor != null);
-    assert(sliderTheme.disabledInactiveTrackColor != null);
-    assert(sliderTheme.activeTrackColor != null);
-    assert(sliderTheme.inactiveTrackColor != null);
-    assert(sliderTheme.thumbShape != null);
-
-    if (sliderTheme.trackHeight == null || sliderTheme.trackHeight! <= 0) {
-      return;
-    }
-
-    final Rect trackRect = getPreferredRect(
-      parentBox: parentBox,
-      offset: offset,
-      sliderTheme: sliderTheme,
-      isEnabled: isEnabled,
-      isDiscrete: isDiscrete,
-    );
-
-    final ColorTween activeTrackColorTween =
-        ColorTween(begin: sliderTheme.disabledActiveTrackColor, end: sliderTheme.activeTrackColor);
-    final ColorTween inactiveTrackColorTween = ColorTween(
-        begin: sliderTheme.disabledInactiveTrackColor, end: sliderTheme.inactiveTrackColor);
-    final Paint activePaint = Paint()
-      ..shader = gradient.createShader(trackRect)
-      ..color = activeTrackColorTween.evaluate(enableAnimation)!;
-    final Paint inactivePaint = Paint()..color = inactiveTrackColorTween.evaluate(enableAnimation)!;
-    final Paint leftTrackPaint;
-    final Paint rightTrackPaint;
-    switch (textDirection) {
-      case TextDirection.ltr:
-        leftTrackPaint = activePaint;
-        rightTrackPaint = inactivePaint;
-        break;
-      case TextDirection.rtl:
-        leftTrackPaint = inactivePaint;
-        rightTrackPaint = activePaint;
-        break;
-    }
-
-    final Radius trackRadius = Radius.circular(trackRect.height / 2);
-    final Radius activeTrackRadius =
-        Radius.circular((trackRect.height + additionalActiveTrackHeight) / 2);
-
-    context.canvas.drawRRect(
-      RRect.fromLTRBAndCorners(
-        trackRect.left,
-        (textDirection == TextDirection.ltr)
-            ? trackRect.top - (additionalActiveTrackHeight / 2)
-            : trackRect.top,
-        thumbCenter.dx,
-        (textDirection == TextDirection.ltr)
-            ? trackRect.bottom + (additionalActiveTrackHeight / 2)
-            : trackRect.bottom,
-        topLeft: (textDirection == TextDirection.ltr) ? activeTrackRadius : trackRadius,
-        bottomLeft: (textDirection == TextDirection.ltr) ? activeTrackRadius : trackRadius,
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 20,
+        height: 20,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          color: tenTenOnePurple,
+          borderRadius: BorderRadius.circular(3),
+        ),
+        child: Icon(
+          icon,
+          color: Colors.white,
+          size: 16,
+        ),
       ),
-      leftTrackPaint,
     );
-    context.canvas.drawRRect(
-      RRect.fromLTRBAndCorners(
-        thumbCenter.dx,
-        (textDirection == TextDirection.rtl)
-            ? trackRect.top - (additionalActiveTrackHeight / 2)
-            : trackRect.top,
-        trackRect.right,
-        (textDirection == TextDirection.rtl)
-            ? trackRect.bottom + (additionalActiveTrackHeight / 2)
-            : trackRect.bottom,
-        topRight: (textDirection == TextDirection.rtl) ? activeTrackRadius : trackRadius,
-        bottomRight: (textDirection == TextDirection.rtl) ? activeTrackRadius : trackRadius,
-      ),
-      rightTrackPaint,
-    );
-
-    final bool showSecondaryTrack = (secondaryOffset != null) &&
-        ((textDirection == TextDirection.ltr)
-            ? (secondaryOffset.dx > thumbCenter.dx)
-            : (secondaryOffset.dx < thumbCenter.dx));
-
-    if (showSecondaryTrack) {
-      final ColorTween secondaryTrackColorTween = ColorTween(
-          begin: sliderTheme.disabledSecondaryActiveTrackColor,
-          end: sliderTheme.secondaryActiveTrackColor);
-      final Paint secondaryTrackPaint = Paint()
-        ..color = secondaryTrackColorTween.evaluate(enableAnimation)!;
-      if (textDirection == TextDirection.ltr) {
-        context.canvas.drawRRect(
-          RRect.fromLTRBAndCorners(
-            thumbCenter.dx,
-            trackRect.top,
-            secondaryOffset.dx,
-            trackRect.bottom,
-            topRight: trackRadius,
-            bottomRight: trackRadius,
-          ),
-          secondaryTrackPaint,
-        );
-      } else {
-        context.canvas.drawRRect(
-          RRect.fromLTRBAndCorners(
-            secondaryOffset.dx,
-            trackRect.top,
-            thumbCenter.dx,
-            trackRect.bottom,
-            topLeft: trackRadius,
-            bottomLeft: trackRadius,
-          ),
-          secondaryTrackPaint,
-        );
-      }
-    }
   }
 }
