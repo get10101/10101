3800252e0246ba9699f72ff8155cbd3ebf8f4c35
Richard Holzeis <richard@holzeis.me>
2024-05-28 17:47:51 +0200

    docs: Add rustdocs to Subscriber api

--- mobile/native/src/event/subscriber.rs
+++ mobile/native/src/event/subscriber.rs
@@ -2,6 +2,10 @@ use crate::event::EventInternal;
 use crate::event::EventType;
 
 pub trait Subscriber {
+    /// Notifies the subcriber about an event. If false is returned the subscriber will be
+    /// unsubscribed, if true the subscriber will remain subscribed.
     fn notify(&self, event: &EventInternal);
+
+    /// Returns a list of events the subscriber wants to subscribe to.
     fn events(&self) -> Vec<EventType>;
 }
