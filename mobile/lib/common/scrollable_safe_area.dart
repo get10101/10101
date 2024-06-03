1e94757859a7e6ec446cf9a4c1147dfa2d5d252a
Restioson <restiosondev@gmail.com>
2024-06-03 14:30:11 +0200

    fix: stop welcome screen overflowing

--- mobile/lib/common/scrollable_safe_area.dart
+++ mobile/lib/common/scrollable_safe_area.dart
@@ -3,14 +3,17 @@ import 'package:flutter/cupertino.dart';
 class ScrollableSafeArea extends StatelessWidget {
   const ScrollableSafeArea({
     required this.child,
+    this.bottom = true,
     Key? key,
   }) : super(key: key);
 
+  final bool bottom;
   final Widget child;
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+        bottom: bottom,
         child: LayoutBuilder(
             builder: (BuildContext context, BoxConstraints viewportConstraints) =>
                 SingleChildScrollView(
